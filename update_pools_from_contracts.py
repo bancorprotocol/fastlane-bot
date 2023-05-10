d5d4d3f5de76d9cbe8675f32a367f5eb5fe19a15
mikewcasale <mikewcasale>
2023-05-10 15:42:32 +0000

    bugfix

--- update_pools_from_contracts.py
+++ update_pools_from_contracts.py
@@ -48,6 +48,7 @@ bot_path = '/Workspace/Repos/mike@bancor.network/carbonbot'
 
 # COMMAND ----------
 
+import os
 ETH_PRIVATE_KEY = dbutils.secrets.get(scope="fastlane", key=f"ETH_PRIVATE_KEY_BE_CAREFUL")
 WEB3_ALCHEMY_PROJECT_ID = os.environ.get('WEB3_ALCHEMY_PROJECT_ID')
 POSTGRES_PASSWORD = dbutils.secrets.get(scope="fastlane", key=f"POSTGRES_PASSWORD")
