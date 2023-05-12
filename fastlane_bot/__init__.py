edb5621b5072f7a4da81f65bed2922b326cce934
sklbancor <109073706+sklbancor@users.noreply.github.com>
2023-05-12 09:07:18 +0100

    init: modified for faster import

--- fastlane_bot/__init__.py
+++ fastlane_bot/__init__.py
@@ -1,5 +1,8 @@
-from .bot import CarbonBot as Bot, __VERSION__, __DATE__
-from .config import Config, ConfigNetwork, ConfigDB, ConfigLogger, ConfigProvider
+#from .bot import CarbonBot as Bot, __VERSION__, __DATE__
+#from .config import Config, ConfigNetwork, ConfigDB, ConfigLogger, ConfigProvider
+
+__VERSION__="3.0-b3skltest"
+__DATE__="12/May/2023"
 
 __version__ = '2.0.0'
 
