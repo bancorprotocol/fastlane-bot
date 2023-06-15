3039b2ce87fa5fdbf1866dcaff019a5ba5f80087
mikewcasale <mike.w.casale@gmail.com>
2023-06-14 22:20:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ load_dotenv()
 @click.option("--n_jobs", default=-1, help="Number of parallel jobs to run")
 @click.option(
     "--exchanges",
-    default="uniswap_v3,uniswap_v2,carbon_v1,bancor_v3",
-    # default="carbon_v1,bancor_v3,uniswap_v3",
+    # default="uniswap_v3,uniswap_v2,carbon_v1,bancor_v3",
+    default="carbon_v1,bancor_v3,uniswap_v3",
     help="Comma separated external exchanges",
 )
 @click.option(
