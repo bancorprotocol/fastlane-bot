77cbc84c61704e6b9f5df514ba6cb600397c7d86
mikewcasale <mike.w.casale@gmail.com>
2023-06-14 20:16:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import json
-import logging
 import os
 import time
 from typing import List, Any, Tuple, Hashable
@@ -45,8 +44,8 @@ load_dotenv()
 @click.option("--n_jobs", default=-1, help="Number of parallel jobs to run")
 @click.option(
     "--exchanges",
-    default="uniswap_v3,uniswap_v2,carbon_v1,bancor_v3",
-    # default="carbon_v1,bancor_v3,uniswap_v3",
+    # default="uniswap_v3,uniswap_v2,carbon_v1,bancor_v3",
+    default="carbon_v1,bancor_v3,uniswap_v3",
     help="Comma separated external exchanges",
 )
 @click.option(
@@ -66,7 +65,6 @@ load_dotenv()
 )
 @click.option(
     "--dbfs_path",
-    # default='data/',
     default='/dbfs/FileStore/tables/carbonbot/logs/',
     help="The Databricks logging path.",
 )
