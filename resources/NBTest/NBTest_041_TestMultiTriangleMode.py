84169f14d40ffcfc663a65982d32b1d709fe46ef
Lesigh-3100 <kevin@bancor.network>
2023-07-19 11:17:43 +0300

    update tests AAA

--- resources/NBTest/NBTest_041_TestMultiTriangleMode.py
+++ resources/NBTest/NBTest_041_TestMultiTriangleMode.py
@@ -46,6 +46,7 @@ require("3.0", __VERSION__)
 
 # # Multi Mode [NB038]
 
+# +
 C = cfg = Config.new(config=Config.CONFIG_MAINNET)
 C.DEFAULT_MIN_PROFIT_BNT = 0.02
 C.DEFAULT_MIN_PROFIT = 0.02
@@ -54,29 +55,17 @@ cfg.DEFAULT_MIN_PROFIT = 0.02
 assert (C.NETWORK == C.NETWORK_MAINNET)
 assert (C.PROVIDER == C.PROVIDER_ALCHEMY)
 setup_bot = CarbonBot(ConfigObj=C)
-
-assert(cfg.DEFAULT_MIN_PROFIT_BNT <= 0.02), f"[TestMultiMode], DEFAULT_MIN_PROFIT_BNT must be <= 0.02 for this Notebook to run, currently set to {cfg.DEFAULT_MIN_PROFIT_BNT}"
-assert(C.DEFAULT_MIN_PROFIT_BNT <= 0.02), f"[TestMultiMode], DEFAULT_MIN_PROFIT_BNT must be <= 0.02 for this Notebook to run, currently set to {cfg.DEFAULT_MIN_PROFIT_BNT}"
-
-
-# +
-
-
 pools = None
 with open('fastlane_bot/data/tests/latest_pool_data_testing.json') as f:
     pools = json.load(f)
 pools = [pool for pool in pools]
-# -
-
 pools[0]
 static_pools = pools
-
 state = pools
 exchanges = list({ex['exchange_name'] for ex in state})
 db = QueryInterface(state=state, ConfigObj=C, exchanges=exchanges)
 setup_bot.db = db
 
-# +
 static_pool_data_filename = "static_pool_data"
 
 static_pool_data = pd.read_csv(f"fastlane_bot/data/{static_pool_data_filename}.csv", low_memory=False)
@@ -88,18 +77,13 @@ tokens = pd.read_csv("fastlane_bot/data/tokens.csv", low_memory=False)
 exchanges = "carbon_v1,bancor_v3,uniswap_v3,uniswap_v2,sushiswap_v2"
 
 exchanges = exchanges.split(",")
-arb_mode = "multi_triangle"
+
 
 alchemy_max_block_fetch = 20
-# -
-
 static_pool_data["cid"] = [
         cfg.w3.keccak(text=f"{row['descr']}").hex()
         for index, row in static_pool_data.iterrows()
     ]
-
-
-# +
 # Filter out pools that are not in the supported exchanges
 static_pool_data = [
     row for index, row in static_pool_data.iterrows()
@@ -107,11 +91,7 @@ static_pool_data = [
 ]
 
 static_pool_data = pd.DataFrame(static_pool_data)
-# -
-
 static_pool_data['exchange_name'].unique()
-
-# +
 # Initialize data fetch manager
 mgr = Manager(
     web3=cfg.w3,
@@ -134,10 +114,6 @@ cfg.logger.info(f"Time taken to add initial pools: {time.time() - start_time}")
 mgr.deduplicate_pool_data()
 cids = [pool["cid"] for pool in mgr.pool_data]
 assert len(cids) == len(set(cids)), "duplicate cid's exist in the pool data"
-
-
-# -
-
 def init_bot(mgr: Manager) -> CarbonBot:
     """
     Initializes the bot.
@@ -160,35 +136,35 @@ def init_bot(mgr: Manager) -> CarbonBot:
         bot.db, QueryInterface
     ), "QueryInterface not initialized correctly"
     return bot
-
-
 bot = init_bot(mgr)
-
 # add data cleanup steps from main.py
 bot.db.handle_token_key_cleanup()
 bot.db.remove_unmapped_uniswap_v2_pools()
 bot.db.remove_zero_liquidity_pools()
 bot.db.remove_unsupported_exchanges()
-
-assert bot.ConfigObj.DEFAULT_MIN_PROFIT_BNT == 0.02
-
 tokens = bot.db.get_tokens()
-
 ADDRDEC = {t.key: (t.address, int(t.decimals)) for t in tokens if not math.isnan(t.decimals)}
-
 flashloan_tokens = bot.setup_flashloan_tokens(None)
 CCm = bot.setup_CCm(None)
-
 pools = db.get_pool_data_with_tokens()
 
-run_full = bot._run(flashloan_tokens=flashloan_tokens, CCm=CCm, arb_mode=arb_mode, data_validator=False, result=bot.XS_ARBOPPS)
+arb_mode = "multi_triangle"
+# -
+
+# ## Test_min_profit
+
+assert(cfg.DEFAULT_MIN_PROFIT_BNT <= 0.02), f"[TestMultiMode], DEFAULT_MIN_PROFIT_BNT must be <= 0.02 for this Notebook to run, currently set to {cfg.DEFAULT_MIN_PROFIT_BNT}"
+assert(C.DEFAULT_MIN_PROFIT_BNT <= 0.02), f"[TestMultiMode], DEFAULT_MIN_PROFIT_BNT must be <= 0.02 for this Notebook to run, currently set to {cfg.DEFAULT_MIN_PROFIT_BNT}"
+assert bot.ConfigObj.DEFAULT_MIN_PROFIT_BNT == 0.02
+
+# ### Test_arb_mode_class
 
 arb_finder = bot._get_arb_finder("multi_triangle")
-
-# ### Test Setup & Dataset
-
 assert arb_finder.__name__ == "ArbitrageFinderTriangleMulti", f"[TestMultiMode] Expected arb_finder class name name = FindArbitrageMultiPairwise, found {arb_finder.__name__}"
 
+# ## Test_combos
+
+arb_finder = bot._get_arb_finder("multi_triangle")
 finder2 = arb_finder(
             flashloan_tokens=flashloan_tokens,
             CCm=CCm,
@@ -196,17 +172,13 @@ finder2 = arb_finder(
             result=bot.AO_TOKENS,
             ConfigObj=bot.ConfigObj,
         )
-
-
-
 combos = finder2.get_combos(flashloan_tokens=flashloan_tokens, CCm=CCm, arb_mode="multi_triangle")
-
-# ### Check Combos
-
 assert len(combos) == 1370, f"[TestMultiMode] Using wrong dataset, expected 1370 combos, found {len(combos)}"
 
-# ### Test Multi Arb Finder
+# ### Test_find_arbitrage
 
+run_full = bot._run(flashloan_tokens=flashloan_tokens, CCm=CCm, arb_mode=arb_mode, data_validator=False, result=bot.XS_ARBOPPS)
+arb_finder = bot._get_arb_finder("multi_triangle")
 finder = arb_finder(
             flashloan_tokens=flashloan_tokens,
             CCm=CCm,
@@ -214,19 +186,22 @@ finder = arb_finder(
             result=bot.AO_CANDIDATES,
             ConfigObj=bot.ConfigObj,
         )
-
 r = finder.find_arbitrage()
-
-
-
 assert len(r) == 40, f"[TestMultiMode] Expected 40 arbs, found {len(r)}"
 assert len(r) == len(run_full), f"[TestMultiMode] Expected arbs from .find_arbitrage: {len(r)} - to match _run: {len(run_full)}"
 
-# ### Test multiple Carbon Curves are being used
+# ### Test_multi_carbon_pools
 
-# +
+arb_finder = bot._get_arb_finder("multi_triangle")
+finder = arb_finder(
+            flashloan_tokens=flashloan_tokens,
+            CCm=CCm,
+            mode="bothin",
+            result=bot.AO_CANDIDATES,
+            ConfigObj=bot.ConfigObj,
+        )
+r = finder.find_arbitrage()
 multi_carbon_count = 0
-
 for arb in r:
     (
             best_profit,
@@ -238,10 +213,18 @@ for arb in r:
     if len(best_trade_instructions_dic) > 3:
         multi_carbon_count += 1
 assert multi_carbon_count > 0, f"[TestMultiMode] Not finding arbs with multiple Carbon curves."
-# -
 
-# ### Test all Carbon Curves are in the same direction
+# ### Test_mono_direction_carbon_curves
 
+arb_finder = bot._get_arb_finder("multi_triangle")
+finder = arb_finder(
+            flashloan_tokens=flashloan_tokens,
+            CCm=CCm,
+            mode="bothin",
+            result=bot.AO_CANDIDATES,
+            ConfigObj=bot.ConfigObj,
+        )
+r = finder.find_arbitrage()
 for arb in r:
     (
             best_profit,
@@ -260,4 +243,3 @@ for arb in r:
             if "-1" in curve['cid']:
                 has_one_curves = True
         assert not has_zero_curves or not has_one_curves, f"[TestMultiMode] Finding Carbon curves in opposite directions - not supported in this mode."
-
