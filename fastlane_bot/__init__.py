8350ddc468da6d563bd9fbf40725e6b4e902b7a5
GitHub Action <action@github.com>
2023-07-24 19:13:26 +0000

    Bump version [skip ci]

--- fastlane_bot/__init__.py
+++ fastlane_bot/__init__.py
@@ -1,7 +1,7 @@
 from .bot import CarbonBot as Bot, __VERSION__, __DATE__
 from .config import Config, ConfigNetwork, ConfigDB, ConfigLogger, ConfigProvider
 
-__version__ = '2.0.0'
+__version__ = '2.1.0'
 
 
 
