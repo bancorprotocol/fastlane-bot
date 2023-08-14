74909310e4d25fd4065910ae8c220e13041a0e76
Mike Casale <46603283+mikewcasale@users.noreply.github.com>
2023-08-14 04:33:06 -0700

    Updates default exchanges in `main.py`

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ load_dotenv()
 @click.option("--n_jobs", default=-1, help="Number of parallel jobs to run")
 @click.option(
     "--exchanges",
-    default="carbon_v1,bancor_v3,uniswap_v3,uniswap_v2,sushiswap_v2",
+    default="carbon_v1,bancor_v3,uniswap_v3,uniswap_v2,sushiswap_v2,pancakeswap_v2",
     help="Comma separated external exchanges. Note that carbon_v1 and bancor_v3 must be included.",
 )
 @click.option(
