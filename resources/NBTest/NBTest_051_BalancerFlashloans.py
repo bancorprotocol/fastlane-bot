b4aa915420113fd560065c1269bdb95a5d2d43bd
Mike Casale <46603283+mikewcasale@users.noreply.github.com>
2023-11-08 01:12:50 -0800

    fix tests

--- resources/NBTest/NBTest_051_BalancerFlashloans.py
+++ resources/NBTest/NBTest_051_BalancerFlashloans.py
@@ -7,9 +7,9 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.14.5
+#       jupytext_version: 1.15.2
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
