44cf184652746952d318ad35ff9d01586c223610
Mike Casale <46603283+mikewcasale@users.noreply.github.com>
2023-11-15 16:38:56 -0800

    Update version_utils.py

--- fastlane_bot/events/version_utils.py
+++ fastlane_bot/events/version_utils.py
@@ -39,5 +39,5 @@ def check_version_requirements():
     installed_version = version("web3")
 
     # Check the version and raise an exception if the requirement is not met
-    if not pkg_version.parse(installed_version) < pkg_version.parse(required_version):
+    if not pkg_version.parse(installed_version) <= pkg_version.parse(required_version):
         raise VersionRequirementError(installed_version, required_version)
