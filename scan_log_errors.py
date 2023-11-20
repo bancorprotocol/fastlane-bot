cc9dbcaad42ba6913ceac83412bc11ae872d6f41
Mike Casale <46603283+mikewcasale@users.noreply.github.com>
2023-11-19 19:52:43 -0800

    cleanup

--- scan_log_errors.py
+++ scan_log_errors.py
@@ -1,12 +1,11 @@
+import os
+import re
 import time
+from datetime import datetime
+from datetime import timedelta
 
 import click
-import re
-from datetime import datetime, timedelta
-import os
-import time
-import re
-from datetime import datetime
+
 
 def is_valid_timestamp_dir(name, time_format):
     try:
@@ -15,12 +14,13 @@ def is_valid_timestamp_dir(name, time_format):
     except ValueError:
         return False
 
+
 def contains_only_one_specific_word(file_path, target_word, other_words):
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         file_contents = file.read()
 
     # Define a regex pattern with word boundaries for the target word
-    target_word_pattern = r'\b' + re.escape(target_word) + r'\b'
+    target_word_pattern = r"\b" + re.escape(target_word) + r"\b"
     target_word_count = len(re.findall(target_word_pattern, file_contents))
     if target_word_count == 0:
         return False
@@ -29,22 +29,29 @@ def contains_only_one_specific_word(file_path, target_word, other_words):
     for word in other_words:
         if word != target_word:
             # Define a regex pattern with word boundaries for each other word
-            word_pattern = r'\b' + re.escape(word) + r'\b'
+            word_pattern = r"\b" + re.escape(word) + r"\b"
             if re.search(word_pattern, file_contents):
                 return False
 
     return True
 
-def find_latest_log_with_string(logs_dir, search_string, non_search_patterns, time_format="%Y%m%d-%H%M%S"):
+
+def find_latest_log_with_string(
+    logs_dir, search_string, non_search_patterns, time_format="%Y%m%d-%H%M%S"
+):
     latest_time = None
     latest_folder = None
 
     for folder_name in os.listdir(logs_dir):
         folder_path = os.path.join(logs_dir, folder_name)
-        if os.path.isdir(folder_path) and is_valid_timestamp_dir(folder_name, time_format):
+        if os.path.isdir(folder_path) and is_valid_timestamp_dir(
+            folder_name, time_format
+        ):
             log_file_path = os.path.join(folder_path, "bot.log")
             if os.path.exists(log_file_path):
-                is_match = contains_only_one_specific_word(log_file_path, search_string, non_search_patterns)
+                is_match = contains_only_one_specific_word(
+                    log_file_path, search_string, non_search_patterns
+                )
                 if is_match:
                     folder_time = datetime.strptime(folder_name, time_format)
                     if not latest_time or folder_time > latest_time:
@@ -54,42 +61,42 @@ def find_latest_log_with_string(logs_dir, search_string, non_search_patterns, ti
     return latest_folder
 
 
-
 def read_log_file(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         return file.read()
 
-def scan_logs_for_opportunity(logs, search_pattern, max_minutes, latest_opportunity_timestamp=None):
+
+def scan_logs_for_opportunity(
+    logs, search_pattern, max_minutes, latest_opportunity_timestamp=None
+):
     time_pattern = r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}),\d{3}"
     search_regex = re.compile(time_pattern + r".*" + search_pattern)
     error_detected = True
 
-    for line in logs.split('\n'):
+    for line in logs.split("\n"):
         match = search_regex.search(line)
         if match:
             last_timestamp = match.group(1)
-            if latest_opportunity_timestamp and datetime.strptime(last_timestamp, '%Y-%m-%d %H:%M:%S') > latest_opportunity_timestamp:
+            if (
+                latest_opportunity_timestamp
+                and datetime.strptime(last_timestamp, "%Y-%m-%d %H:%M:%S")
+                > latest_opportunity_timestamp
+            ):
                 error_detected = False
                 print(f"Opportunity found at {last_timestamp}")
                 break
 
     if error_detected:
-        raise ValueError(f"Opportunity not found within {max_minutes} minutes interval.")
+        raise ValueError(
+            f"Opportunity not found within {max_minutes} minutes interval."
+        )
 
 
 @click.command()
-@click.option('--logs_directory',
-                default="logs",
-              type=str)
-@click.option('--interval',
-                default=30,
-                type=int)
-@click.option('--search_pattern',
-                default="multi_pairwise_all",
-                type=str)
-@click.option('--max_minutes',
-                default=10,
-                type=int)
+@click.option("--logs_directory", default="logs", type=str)
+@click.option("--interval", default=30, type=int)
+@click.option("--search_pattern", default="multi_pairwise_all", type=str)
+@click.option("--max_minutes", default=10, type=int)
 def main(logs_directory, interval, search_pattern, max_minutes):
 
     all_patterns = [
@@ -114,7 +121,9 @@ def main(logs_directory, interval, search_pattern, max_minutes):
 
     search_pattern = f"arb_mode: {search_pattern}"
 
-    latest_folder = find_latest_log_with_string(logs_directory, search_pattern, non_search_patterns)
+    latest_folder = find_latest_log_with_string(
+        logs_directory, search_pattern, non_search_patterns
+    )
 
     if latest_folder:
         print(f"Latest folder containing '{search_pattern}': {latest_folder}")
@@ -127,21 +136,31 @@ def main(logs_directory, interval, search_pattern, max_minutes):
         last_timestamp = datetime.now() - timedelta(minutes=max_minutes)
         try:
             logs = read_log_file(f"{latest_folder}/bot.log")
-            scan_logs_for_opportunity(logs, r"Opportunity with profit:", max_minutes, last_timestamp)
+            scan_logs_for_opportunity(
+                logs, r"Opportunity with profit:", max_minutes, last_timestamp
+            )
             print("Everything is OK...")
         except ValueError as e:
-            next_latest_folder = find_latest_log_with_string(logs_directory, search_pattern, non_search_patterns)
+            next_latest_folder = find_latest_log_with_string(
+                logs_directory, search_pattern, non_search_patterns
+            )
             if next_latest_folder and next_latest_folder != latest_folder:
-                print(f"Latest folder containing '{search_pattern}': {next_latest_folder}")
+                print(
+                    f"Latest folder containing '{search_pattern}': {next_latest_folder}"
+                )
                 latest_folder = next_latest_folder
                 continue
             else:
                 print(e)
                 break
         except Exception as ex:
-            next_latest_folder = find_latest_log_with_string(logs_directory, search_pattern, non_search_patterns)
+            next_latest_folder = find_latest_log_with_string(
+                logs_directory, search_pattern, non_search_patterns
+            )
             if next_latest_folder and next_latest_folder != latest_folder:
-                print(f"Latest folder containing '{search_pattern}': {next_latest_folder}")
+                print(
+                    f"Latest folder containing '{search_pattern}': {next_latest_folder}"
+                )
                 latest_folder = next_latest_folder
                 continue
             else:
@@ -150,5 +169,5 @@ def main(logs_directory, interval, search_pattern, max_minutes):
         time.sleep(interval)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
