e974b9b7548d76d0ea714b57c796927579afedbc
Lesigh-3100 <kevin@bancor.network>
2023-11-28 15:55:50 +0200

    Reinstate WETH Carbon filter when contract version is less than 10

--- fastlane_bot/bot.py
+++ fastlane_bot/bot.py
@@ -1220,6 +1220,17 @@ class CarbonBot(CarbonBotBase):
         """
         if CCm is None:
             CCm = self.get_curves()
+            if self.ConfigObj.ARB_CONTRACT_VERSION < 10:
+                filter_out_weth = [
+                    x
+                    for x in CCm
+                    if (x.params.exchange == "carbon_v1")
+                       & (
+                               (x.params.tkny_addr == self.ConfigObj.WETH_ADDRESS)
+                               or (x.params.tknx_addr == self.ConfigObj.WETH_ADDRESS)
+                       )
+                ]
+                CCm = CPCContainer([x for x in CCm if x not in filter_out_weth])
         return CCm
 
     def run_continuous_mode(
