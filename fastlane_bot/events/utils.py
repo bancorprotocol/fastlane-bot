f2a77887889ae63d50775f28d49b1c48ae0c25df
Lesigh-3100 <kevin@bancor.network>
2023-12-06 19:07:59 +0200

    Fix tokens on Base & remove extra columns from token info

--- fastlane_bot/events/utils.py
+++ fastlane_bot/events/utils.py
@@ -255,7 +255,7 @@ def get_static_data(
     tokens_filepath = os.path.join(base_path, "tokens.csv")
     if not os.path.exists(tokens_filepath):
         df = pd.DataFrame(
-            columns=["key", "symbol", "name", "address", "decimals", "blockchain"]
+            columns=["address", "symbol", "decimals"]
         )
         df.to_csv(tokens_filepath)
     tokens = read_csv_file(tokens_filepath)
