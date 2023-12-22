7dfda8322da30b881109fefa9774563783054a62
NIXBNT <88088888+NIXBNT@users.noreply.github.com>
2023-12-22 20:03:39 +1100

    Update txhelpers.py

--- fastlane_bot/helpers/txhelpers.py
+++ fastlane_bot/helpers/txhelpers.py
@@ -946,7 +946,7 @@ class TxHelpers:
             transaction hash
         """
         current_gas_price = self.web3.eth.get_block("pending").get("baseFeePerGas")
-        max_priority = int(self.get_max_priority_fee_per_gas_alchemy()) if self.ConfigObj.NETWORK in ["ethereum", "coinbase_bse"] else 0
+        max_priority = int(self.get_max_priority_fee_per_gas_alchemy()) if self.ConfigObj.NETWORK in ["ethereum", "coinbase_base"] else 0
 
         token_contract = self.web3.eth.contract(address=token_address, abi=ERC20_ABI)
         try:
