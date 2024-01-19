e30b71e56cefdc9a87a02340c0e0f18867f20a03
sklbancor <109073706+sklbancor@users.noreply.github.com>
2024-01-19 15:29:40 +0000

    functions v0.9.1
    Update functions.py
    
    Update functions.py
    
    Update functions.py

--- fastlane_bot/tools/invariants/functions.py
+++ fastlane_bot/tools/invariants/functions.py
@@ -4,8 +4,8 @@ object representing a function y = f(x; params)
 (c) Copyright Bprotocol foundation 2024. 
 Licensed under MIT
 """
-__VERSION__ = '0.9'
-__DATE__ = "18/Jan/2024"
+__VERSION__ = '0.9.1'
+__DATE__ = "19/Jan/2024"
 
 from dataclasses import dataclass, asdict
 from abc import ABC, abstractmethod
@@ -16,6 +16,15 @@ from inspect import signature
 
 from .vector import DictVector
 from .kernel import Kernel
+
+def fmt(dct_or_list, format_string=None, as_float=True):
+    """format dct key=>value -> key: str"""
+    format_string = format_string or ".4f"
+    fmt2 = (lambda x: float(x)) if as_float else lambda x: x
+    if isinstance(dct_or_list, dict):
+        return {key: fmt2(format(value, format_string)) for key, value in dct_or_list.items()}
+    else: 
+        return [fmt2(format(value, format_string)) for value in dct_or_list]
 
 @dataclass(frozen=True)
 class Function(ABC):
@@ -114,11 +123,16 @@ class Function(ABC):
         """returns the second derivative as a function object"""
         return Derivative2Function(self, precision=precision)
     
-    def params(self):
+    def params(self, *, classname=False):
         """
         returns the parameters of the function as a dictionary
+        
+        :classname:    if True, includes the class name in the dict (default: False)
         """
-        return asdict(self)
+        result = asdict(self)
+        if classname:
+            result["_classname"] = self.__class__.__name__
+        return result
     
     def update(self, **kwargs):
         """
@@ -127,6 +141,8 @@ class Function(ABC):
         :kwargs:    parameters to update
         """
         params = {**self.params(), **kwargs}
+        try:    del params["_classname"]
+        except  KeyError: pass
         return self.__class__(**params)
     
     def __call__(self, x):
@@ -134,6 +150,46 @@ class Function(ABC):
         alias for self.f(x)
         """
         return self.f(x)
+    
+    def plot(self, x_min, x_max, func=None, *, steps=None, title=None, xlabel=None, ylabel=None, grid=True, show=True, **kwargs):
+        """
+        plots the function
+        
+        :x_min:     lower bound
+        :x_max:     upper bound
+        :func:      function to plot (default: self.f)
+        :steps:     number of steps (default: np.linspace defaults)
+        :show:      whether to call plt.show() (default: True)
+        :grid:      whether to show a grid (default: True)
+        :returns:   the result of plt.plot
+        """
+        func = func or self.f
+        x = np.linspace(x_min, x_max, steps) if steps else np.linspace(x_min, x_max)
+        y = [func(x) for x in x]
+        plot = plt.plot(x, y, **kwargs)
+        if title: plt.title(title)
+        if xlabel: plt.xlabel(xlabel)
+        if ylabel: plt.ylabel(ylabel)
+        if grid: plt.grid(True)
+        if show: plt.show()
+        return plot
+    
+    def wrap(self, fv_or_kernel=None):
+        """
+        wraps this function in a FunctionVector
+        
+        :fv_or_kernel:  either a FunctionVector or a Kernel
+        :returns:       FunctionVector(self, kernel=kernel)
+        """
+        if isinstance(fv_or_kernel, FunctionVector):
+            kernel = fv_or_kernel.kernel
+        else:
+            kernel = fv_or_kernel
+        if kernel is None:
+            kernel = Kernel()
+        return FunctionVector({self: 1}, kernel=kernel)
+    
+    
 
 @dataclass(frozen=True)
 class QuadraticFunction(Function):
@@ -214,9 +270,81 @@ class FunctionVector(DictVector):
             self.kernel = Kernel()  
             
     def wrap(self, func):
-        """creates a FunctionVector from a function using the same kernel as self"""
-        assert isinstance(func, Function), "func must be of type Function"
-        return self.__class__({func: 1}, kernel=self.kernel)
+        """creates a FunctionVector from a Function (or iterable of functions) using the same kernel as self"""
+        try:
+            return self.__class__({f: 1 for f in func}, kernel=self.kernel)
+        except:
+            assert isinstance(func, Function), "func must be of type Function"
+            return self.__class__({func: 1}, kernel=self.kernel)
+    
+    def functions(self):
+        """returns all functions in self as a list"""
+        return list(self.vec.keys())
+    
+    def function(self, i=0):
+        """returns the i'th function in self"""
+        return self.functions()[i]
+    
+    def params(self, index=None, *, as_dict=None, classname=None):
+        """
+        retrieve params of the underlying function(s)
+        
+        :index:         the index (0,1,2...) of the item to be retrieved
+        :as_dict:       if True, returns items as dict, otherwise as list
+        :classname:     if True, includes the class name in the params
+        
+        
+        index       as_dict         Action
+        ---         ---             ---
+        None        True            return all as dict
+        None        False           return all as list
+        None        None            list as_dict = True
+        int         True            return params of item i (key => params)
+        int         False           ditto, params only
+        int         None            like as_dict = True
+        """
+        if index is None :
+            # return all as dict
+            as_dict = as_dict if not as_dict is None else False
+            classname = classname if not classname is None else True
+            result = {f: f.params(classname=classname) for f in self.functions()}
+            if as_dict:
+                return result
+            return list(result.values())
+        
+        else:
+            # index given => return params of item i
+            as_dict = as_dict if not as_dict is None else False
+            classname = classname if not classname is None else True
+            f = self.function(index)
+            if as_dict:
+                return {f: f.params(classname=classname)}
+            return f.params(classname=classname)
+
+        
+    def update(self, params=None, index=None, *, key=None, **kwargs):
+        """
+        creates a copy of the FunctionVector, with the relevant functions updated
+        
+        :index:     if not None, only updates the i'th function
+        :params:    the parameters to be updated (single dict or list of dicts)
+        :key:       if not None, only updates the function with the given key
+        :returns:   the newly created, updated FunctionVector
+        """
+        if index is None and key is None and len(self.vec) == 1:
+            index = 0
+        if isinstance(params, list) or isinstance(params, tuple):
+            assert index is None and key is None, "index and key must be None if params is a list"
+            raise NotImplementedError("update with list of params not implemented yet")
+        else:
+            if params is None: params = dict()
+            params = {**params, **kwargs}
+            assert not index is None or not key is None, "exactly one of index or key must be given"
+            assert not(not index is None and not key is None), "can't give both index and key"
+            assert key is None, "key not implemented yet"
+            funcs = self.functions()
+            funcs[index] = funcs[index].update(**params)
+            return self.wrap(funcs)
             
     def __eq__(self, other):
         funcs_eq = super().__eq__(other)
@@ -319,28 +447,44 @@ class FunctionVector(DictVector):
         """integrates self.f using the kernel [convenience access for integrate_func(func=None)]"""
         return self.integrate_func(func=self.f, steps=steps, method=method) 
     
-    def distance2(self, func=None, *, steps=None, method=None):
-        """calculates the distance^2 between self and func (L2 norm)"""
-        #if func is None: func = lambda x: 0
-        #kernel = self.kernel if not ignore_kernel else lambda x: 1
-        #f = lambda x: (self.f(x)-func(x))**2 * kernel(x)
+    def dist2_L2(self, func=None, *, steps=None, method=None):
+        """
+        calculates the L2 distance^2 between self and func (L2 norm squared)
+        """
         if not func is None:
             f = lambda x: (self.f(x)-func(x))**2 * self.kernel(x)
         else:
             f = lambda x: self.f(x)**2 * self.kernel(x)
         return self.integrate_func(func=f, steps=steps, method=method)
     
-    def distance(self, func=None, *, steps=None, method=None):
+    def dist_L2(self, func=None, *, steps=None, method=None):
         """calculates the distance between self and func (L2 norm)"""
-        return m.sqrt(self.distance2(func=func, steps=steps, method=method))
-
-    def norm2(self, *, steps=None, method=None):
-        """calculates the norm^2 of self (L2 norm)"""
-        return self.distance2(func=None, steps=steps, method=method)
+        return m.sqrt(self.dist2_L2(func=func, steps=steps, method=method))
     
-    def norm(self, *, steps=None, method=None):
-        """calculates the norm of self (L2 norm)"""
+    def dist_L1(self, func=None, *, steps=None, method=None):
+        """
+        calculates the L1 distance between self and func (L1 norm)
+        """
+        if not func is None:
+            f = lambda x: (abs(self.f(x)-func(x))) * self.kernel(x)
+        else:
+            f = lambda x: abs(self.f(x)) * self.kernel(x)
+        return self.integrate_func(func=f, steps=steps, method=method)
+
+    def norm2_L2(self, *, steps=None, method=None):
+        """calculates the L2 norm squared of self"""
+        return self.dist2_L2(func=None, steps=steps, method=method)
+    norm2 = norm2_L2
+    
+    def norm_L2(self, *, steps=None, method=None):
+        """calculates the L2 norm of self"""
         return m.sqrt(self.norm2(steps=steps, method=method))
+    norm = norm_L2
+    
+    def norm_L1(self, *, steps=None, method=None):
+        """calculates the L1 norm of self"""
+        return self.dist_L1(func=None, steps=steps, method=method)
+    norm1 = norm_L1
     
     def goalseek(self, target=0, *, x0=1):
         """
@@ -357,6 +501,33 @@ class FunctionVector(DictVector):
         for i in range(iterations):
             y = func(x)
             m = (func(x+h)-func(x-h)) / (2*h)
+            x = x + (target-y)/m
+            if abs(func(x)-target) < tolerance:
+                break
+        if abs(func(x)-target) > tolerance:
+            raise ValueError(f"gradient descent failed to converge on {target}")
+        return x
+    
+    def goalseek(self, func=None, target=0, *, x0=1, iterations=None, tolerance=None, eta=None, h=None):
+        """alias for self.goalseek, but with defaults for func=self.f"""
+        func = func or self.f
+        return self.goalseek_cls(func, target=target, x0=x0, iterations=iterations, tolerance=tolerance, eta=eta, h=h)
+    
+    @classmethod
+    def goalseek_cls(cls, func, target=0, *, x0=1, iterations=None, tolerance=None, eta=None, h=None):
+        """
+        very simple gradient descent implementation for a goal seek (classmethod)
+        
+        :target:    target value (default: 0)
+        :x0:        starting estimate
+        """
+        x = x0
+        iterations = iterations or cls.GS_ITERATIONS
+        tolerance = tolerance or cls.GS_TOLERANCE
+        hh = x0*(eta or cls.GS_ETA) if x0 else (h or cls.GS_H)
+        for i in range(iterations):
+            y = func(x)
+            m = (func(x+hh)-func(x-hh)) / (2*hh)
             x = x + (target-y)/m
             if abs(func(x)-target) < tolerance:
                 break
@@ -412,7 +583,8 @@ class FunctionVector(DictVector):
                 iterations=None, 
                 tolerance=None, 
                 deriv_h=None, 
-                return_path=False
+                return_path=False,
+                verbose = False,
         ):
         """
         minimizes the function `func` using gradient descent (multiple dimensions)
@@ -426,6 +598,7 @@ class FunctionVector(DictVector):
         :return_path:       if True, returns the entire optimization path (list of np.array)
                             as well as the last dfdx (np.array); in this case, the result is 
                             the last element of the path
+        :verbose:           if True, prints verbose output during optimization
         
         *if x0 is np=array-like or None, then func will be called with positional arguments
         and the result will be returned as an np.array. if x0 is a dict, then func will be
@@ -450,16 +623,24 @@ class FunctionVector(DictVector):
         
         # that's where the magic happens
         _minimize = cls._minimize_dct if isinstance(x0, dict) else cls._minimize_lst
-        path, dfdx, norm2_dfdx = _minimize(func, x0, learning_rate, iterations, tol_squared, deriv_h)
+        path, dfdx, norm2_dfdx = _minimize(func, x0, learning_rate, iterations, tol_squared, deriv_h, verbose)
         
+        if verbose:
+            print(f"[minimize] algorithm returned, norm={m.sqrt(norm2_dfdx)}")
         if return_path:
+            if verbose:
+                print(f"[minimize] return path (len={len(path)}), final point={path[-1]})")
             return path, dfdx
         if norm2_dfdx < tol_squared:
+            if verbose:
+                print(f"[minimize] converged in {len(path)} iterations, norm={m.sqrt(norm2_dfdx):.4f}, x={path[-1]})")
             return path[-1]
+        if verbose:
+            print(f"[minimize] did not converged in {len(path)} iterations, norm={m.sqrt(norm2_dfdx):.4f}, x={path[-1]})")
         raise ValueError(f"gradient descent failed to converge")
     
     @classmethod
-    def _minimize_lst(cls, func, x0, learning_rate, iterations, tol_squared, deriv_h):
+    def _minimize_lst(cls, func, x0, learning_rate, iterations, tol_squared, deriv_h, verbose):
         """
         executes the minimize algorithm when the x-values are in a list
         
@@ -468,41 +649,49 @@ class FunctionVector(DictVector):
         x = np.array(x0, dtype=float)
         n = len(x)
         path = [tuple(x)]
-        #print(f"[_minimize_lst] x0={x}")
+        if verbose:
+            print(f"[_minimize_lst] x0={fmt(x, '.4f')}")
+        
         for iteration in range(iterations):
             f0 = func(*x)
             dfdx = np.array([
                 (func( *(x+deriv_h*cls.e_i(i, n)) ) - f0) / deriv_h 
                 for i in range(len(x))
             ])
-            x -= learning_rate * dfdx
+            dx = learning_rate * dfdx
+            x -= dx
             path.append(tuple(x))
             #print(f"[_minimize_lst] {iteration}: adding x={x}, gradient={dfdx}")  
             norm2_dfdx = np.dot(dfdx, dfdx)
+            if verbose:
+                print(f"[_minimize_lst] {iteration}: norm={m.sqrt(norm2_dfdx):.4f}\nx={fmt(x, '.4f')}\ngradient={fmt(dfdx, '.4f')}\ndx={fmt(dx, '.4f')}\n\n")
             if norm2_dfdx < tol_squared:
                 break
         return path, dfdx, norm2_dfdx
     
     @classmethod
-    def _minimize_dct(cls, func, x0, learning_rate, iterations, tol_squared, deriv_h):
+    def _minimize_dct(cls, func, x0, learning_rate, iterations, tol_squared, deriv_h, verbose):
         """
         executes the minimize algorithm when the x-values are in a dict
         
         :returns:  tuple(path, dfdx, norm2_dfdx); result is path[-1]
         """
         x = {**x0}
-        path = [{**x}   ]
-        #print(f"[_minimize_dct] x0={x}")
+        path = [{**x}]
+        if verbose:
+            print(f"[_minimize_dct] x0={x}")
         for iteration in range(iterations):
             f0 = func(**x)
             dfdx = {
                 k: (func( **(cls.bump(x, k, deriv_h)) ) - f0) / deriv_h 
                 for k in x.keys()
             }
-            x = {k: x[k] - learning_rate * dfdx[k] for k in x.keys()}
+            dx = {k: learning_rate * dfdx[k] for k in x.keys()}
+            x = {k: x[k] - dx[k] for k in x.keys()}
             path.append({**x})
-            #print(f"[_minimize_dct] {iteration}: adding x={x}, gradient={dfdx}") 
             norm2_dfdx = sum(vv**2 for vv in dfdx.values())  
+            if verbose:
+                print(f"[_minimize_dct] {iteration}: norm={m.sqrt(norm2_dfdx):.2f}\nx={fmt(x, '.4f')}\ngradient={fmt(dfdx, '.4f')}\ndx={fmt(dx, '.4f')}\n\n")
             if norm2_dfdx < tol_squared:
                 break
         return path, dfdx, norm2_dfdx
@@ -522,7 +711,7 @@ class FunctionVector(DictVector):
         
         def optimizer_func(**params):
             func1 = func.update(**params)
-            return self.distance2(func=func1)
+            return self.dist2_L2(func=func1)
         
         return self.minimize(optimizer_func, x0=params0, **kwargs)
         
@@ -569,6 +758,7 @@ class FunctionVector(DictVector):
         return dict(kernel=self.kernel)
     
 minimize = FunctionVector.minimize
+goalseek = FunctionVector.goalseek_cls
     
     
     
