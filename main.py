43c9ad81fdace5f608ad7b39003492dd208af962
Mike Casale <46603283+mikewcasale@users.noreply.github.com>
2024-01-23 04:17:49 -0800

    Modifies the command-line arguments to remove the click library as a dependency

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ import time
 from typing import List
 from traceback import format_exc
 
-import click
 import pandas as pd
 from dotenv import load_dotenv
 from web3 import Web3, HTTPProvider
@@ -62,238 +61,13 @@ from fastlane_bot.events.utils import (
 )
 from fastlane_bot.utils import find_latest_timestamped_folder
 from run_blockchain_terraformer import terraform_blockchain
+import argparse
 
 
 load_dotenv()
 
 
-@click.command()
-@click.option(
-    "--cache_latest_only",
-    default=True,
-    type=bool,
-    help="Set to True for production. Set to False for " "testing / debugging",
-)
-@click.option(
-    "--backdate_pools",
-    default=False,
-    type=bool,
-    help="Set to False for faster testing / debugging",
-)
-@click.option(
-    "--static_pool_data_filename",
-    default="static_pool_data",
-    help="Filename of the static pool data.",
-)
-@click.option(
-    "--arb_mode",
-    default="multi_pairwise_all",
-    help="See arb_mode in bot.py",
-    type=click.Choice(
-        [
-            "single",
-            "multi",
-            "triangle",
-            "multi_triangle",
-            "b3_two_hop",
-            "multi_pairwise_pol",
-            "multi_pairwise_all",
-        ]
-    ),
-)
-@click.option(
-    "--flashloan_tokens",
-    default=f"{T.LINK},{T.NATIVE_ETH},{T.BNT},{T.WBTC},{T.DAI},{T.USDC},{T.USDT},{T.WETH}",
-    type=str,
-    help="The --flashloan_tokens flag refers to those token denominations which the bot can take a flash loan in."
-    "If you override the default, "
-    "the search space is decreased for all modes, including the b3_two_hop mode (assuming that "
-    "--limit_bancor3_flashloan_tokens=True).",
-)
-@click.option("--n_jobs", default=-1, help="Number of parallel jobs to run")
-@click.option(
-    "--exchanges",
-    default="carbon_v1,bancor_v3,bancor_v2,bancor_pol,uniswap_v3,uniswap_v2,sushiswap_v2,balancer,pancakeswap_v2,pancakeswap_v3",
-    help="Comma separated external exchanges. Note that carbon_v1 and bancor_v3 must be included on Ethereum. To include known forks for Uniswap V2/3, use uniswap_v2_forks or uniswap_v3_forks",
-)
-@click.option(
-    "--polling_interval",
-    default=1,
-    help="Polling interval in seconds",
-)
-@click.option(
-    "--alchemy_max_block_fetch",
-    default=2000,
-    help="Max number of blocks to fetch from alchemy",
-)
-@click.option(
-    "--reorg_delay",
-    default=0,
-    help="Number of blocks delayed to avoid reorgs",
-)
-@click.option(
-    "--logging_path",
-    default="",
-    help="The logging path.",
-)
-@click.option(
-    "--loglevel",
-    default="INFO",
-    type=click.Choice(["DEBUG", "INFO", "WARNING", "ERROR"]),
-    help="The logging level.",
-)
-@click.option(
-    "--use_cached_events",
-    default=False,
-    type=bool,
-    help="Set to True for debugging / testing. Set to False for production.",
-)
-@click.option(
-    "--run_data_validator",
-    default=False,
-    type=bool,
-    help="Set to True for debugging / testing. Set to False for production.",
-)
-@click.option(
-    "--randomizer",
-    default=3,
-    type=int,
-    help="Set to the number of arb opportunities to pick from.",
-)
-@click.option(
-    "--limit_bancor3_flashloan_tokens",
-    default=True,
-    type=bool,
-    help="Only applies if arb_mode is `bancor_v3` or `b3_two_hop`. Set to False to allow the flashloan_tokens "
-    "parameter to be overwritten as all tokens supported by Bancor v3.",
-)
-@click.option(
-    "--default_min_profit_gas_token",
-    default="0.01",
-    type=str,
-    help="Set to the default minimum profit in gas token. This should be reasonably high to avoid losses from gas fees.",
-)
-@click.option(
-    "--timeout",
-    default=None,
-    type=int,
-    help="Set to the timeout in seconds. Set to None for no timeout.",
-)
-@click.option(
-    "--target_tokens",
-    default=None,
-    type=str,
-    help="A comma-separated string of tokens to target. Use None to target all tokens. Use `flashloan_tokens` to "
-    "target only the flashloan tokens.",
-)
-@click.option(
-    "--replay_from_block",
-    default=None,
-    type=int,
-    help="Set to a block number to replay from that block. (For debugging / testing). A valid Tenderly account and "
-    "configuration is required.",
-)
-@click.option(
-    "--tenderly_fork_id",
-    default=None,
-    type=str,
-    help="Set to a Tenderly fork id. (For debugging / testing). A valid Tenderly account and configuration is required.",
-)
-@click.option(
-    "--tenderly_event_exchanges",
-    default="pancakeswap_v2,pancakeswap_v3",
-    type=str,
-    help="A comma-separated string of exchanges to include for the Tenderly event fetcher.",
-)
-@click.option(
-    "--increment_time",
-    default=1,
-    type=int,
-    help="If tenderly_fork_id is set, this is the number of seconds to increment the fork time by for each iteration.",
-)
-@click.option(
-    "--increment_blocks",
-    default=1,
-    type=int,
-    help="If tenderly_fork_id is set, this is the number of blocks to increment the block number by for each iteration.",
-)
-@click.option(
-    "--blockchain",
-    default="ethereum",
-    help="""A blockchain from the list. Blockchains not in this list do not have a deployed Fast Lane contract and 
-    are not supported.""",
-    type=click.Choice(["ethereum", "coinbase_base"]),
-)
-@click.option(
-    "--pool_data_update_frequency",
-    default=-1,
-    type=int,
-    help="How frequently pool data should be updated, in main loop iterations.",
-)
-@click.option(
-    "--use_specific_exchange_for_target_tokens",
-    default=None,
-    type=str,
-    help="If an exchange is specified, this will limit the scope of tokens to the tokens found on the exchange",
-)
-@click.option(
-    "--prefix_path",
-    default="",
-    type=str,
-    help="Prefixes the path to the write folders (used for deployment)",
-)
-@click.option(
-    "--version_check_frequency",
-    default=1,
-    type=int,
-    help="How frequently pool data should be updated, in main loop iterations.",
-)
-@click.option(
-    "--self_fund",
-    default=False,
-    type=bool,
-    help="If True, the bot will attempt to submit arbitrage transactions using funds in your wallet when possible.",
-)
-@click.option(
-    "--read_only",
-    default=False,
-    type=bool,
-    help="If True, the bot will skip all operations which write to disk. Use this flag if you're running the bot in "
-         "an environment with restricted write permissions.",
-)
-def main(
-    cache_latest_only: bool,
-    backdate_pools: bool,
-    arb_mode: str,
-    flashloan_tokens: str,
-    n_jobs: int,
-    exchanges: str,
-    polling_interval: int,
-    alchemy_max_block_fetch: int,
-    static_pool_data_filename: str,
-    reorg_delay: int,
-    logging_path: str,
-    loglevel: str,
-    use_cached_events: bool,
-    run_data_validator: bool,
-    randomizer: int,
-    limit_bancor3_flashloan_tokens: bool,
-    default_min_profit_gas_token: str,
-    timeout: int,
-    target_tokens: str,
-    replay_from_block: int,
-    tenderly_fork_id: str,
-    tenderly_event_exchanges: str,
-    increment_time: int,
-    increment_blocks: int,
-    blockchain: str,
-    pool_data_update_frequency: int,
-    use_specific_exchange_for_target_tokens: str,
-    prefix_path: str,
-    version_check_frequency: int,
-    self_fund: bool,
-    read_only: bool,
-):
+def main():
     """
     The main entry point of the program. It sets up the configuration, initializes the web3 and Base objects,
     adds initial pools to the Base and then calls the `run` function.
@@ -332,6 +106,74 @@ def main(
         read_only (bool): If True, the bot will skip all operations which write to disk. Use this flag if you're running the bot in an environment with restricted write permissions.
 
     """
+    parser = argparse.ArgumentParser(description='Command-line tool options')
+
+    parser.add_argument("--cache_latest_only", default=True, type=bool, help="Set to True for production. Set to False for testing / debugging")
+    parser.add_argument("--backdate_pools", default=False, type=bool, help="Set to False for faster testing / debugging")
+    parser.add_argument("--static_pool_data_filename", default="static_pool_data", help="Filename of the static pool data.")
+    parser.add_argument("--arb_mode", default="multi_pairwise_all", help="See arb_mode in bot.py", choices=["single", "multi", "triangle", "multi_triangle", "b3_two_hop", "multi_pairwise_pol", "multi_pairwise_all"])
+    parser.add_argument("--flashloan_tokens", default=f"{T.LINK},{T.NATIVE_ETH},{T.BNT},{T.WBTC},{T.DAI},{T.USDC},{T.USDT},{T.WETH}", type=str, help="The --flashloan_tokens flag refers to those token denominations which the bot can take a flash loan in.")
+    parser.add_argument("--n_jobs", default=-1, help="Number of parallel jobs to run")
+    parser.add_argument("--exchanges", default="carbon_v1,bancor_v3,bancor_v2,bancor_pol,uniswap_v3,uniswap_v2,sushiswap_v2,balancer,pancakeswap_v2,pancakeswap_v3", help="Comma separated external exchanges.")
+    parser.add_argument("--polling_interval", default=1, help="Polling interval in seconds")
+    parser.add_argument("--alchemy_max_block_fetch", default=2000, help="Max number of blocks to fetch from alchemy")
+    parser.add_argument("--reorg_delay", default=0, help="Number of blocks delayed to avoid reorgs")
+    parser.add_argument("--logging_path", default="", help="The logging path.")
+    parser.add_argument("--loglevel", default="INFO", choices=["DEBUG", "INFO", "WARNING", "ERROR"], help="The logging level.")
+    parser.add_argument("--use_cached_events", default=False, type=bool, help="Set to True for debugging / testing. Set to False for production.")
+    parser.add_argument("--run_data_validator", default=False, type=bool, help="Set to True for debugging / testing. Set to False for production.")
+    parser.add_argument("--randomizer", default=3, type=int, help="Set to the number of arb opportunities to pick from.")
+    parser.add_argument("--limit_bancor3_flashloan_tokens", default=True, type=bool, help="Only applies if arb_mode is `bancor_v3` or `b3_two_hop`.")
+    parser.add_argument("--default_min_profit_gas_token", default="0.01", type=str, help="Set to the default minimum profit in gas token.")
+    parser.add_argument("--timeout", default=None, type=int, help="Set to the timeout in seconds. Set to None for no timeout.")
+    parser.add_argument("--target_tokens", default=None, type=str, help="A comma-separated string of tokens to target.")
+    parser.add_argument("--replay_from_block", default=None, type=int, help="Set to a block number to replay from that block.")
+    parser.add_argument("--tenderly_fork_id", default=None, type=str, help="Set to a Tenderly fork id.")
+    parser.add_argument("--tenderly_event_exchanges", default="pancakeswap_v2,pancakeswap_v3", type=str, help="A comma-separated string of exchanges to include for the Tenderly event fetcher.")
+    parser.add_argument("--increment_time", default=1, type=int, help="If tenderly_fork_id is set, this is the number of seconds to increment the fork time by for each iteration.")
+    parser.add_argument("--increment_blocks", default=1, type=int, help="If tenderly_fork_id is set, this is the number of blocks to increment the block number by for each iteration.")
+    parser.add_argument("--blockchain", default="ethereum", help="A blockchain from the list. Blockchains not in this list do not have a deployed Fast Lane contract and are not supported.", choices=["ethereum", "coinbase_base"])
+    parser.add_argument("--pool_data_update_frequency", default=-1, type=int, help="How frequently pool data should be updated, in main loop iterations.")
+    parser.add_argument("--use_specific_exchange_for_target_tokens", default=None, type=str, help="If an exchange is specified, this will limit the scope of tokens to the tokens found on the exchange")
+    parser.add_argument("--prefix_path", default="", type=str, help="Prefixes the path to the write folders (used for deployment)")
+    parser.add_argument("--version_check_frequency", default=1, type=int, help="How frequently pool data should be updated, in main loop iterations.")
+    parser.add_argument("--self_fund", default=False, type=bool, help="If True, the bot will attempt to submit arbitrage transactions using funds in your wallet when possible.")
+    parser.add_argument("--read_only", default=True, type=bool, help="If True, the bot will skip all operations which write to disk. Use this flag if you're running the bot in an environment with restricted write permissions.")
+
+    args = parser.parse_args()
+
+    cache_latest_only = args.cache_latest_only
+    backdate_pools = args.backdate_pools
+    arb_mode = args.arb_mode
+    flashloan_tokens = args.flashloan_tokens
+    n_jobs = args.n_jobs
+    exchanges = args.exchanges
+    polling_interval = args.polling_interval
+    alchemy_max_block_fetch = args.alchemy_max_block_fetch
+    static_pool_data_filename = args.static_pool_data_filename
+    reorg_delay = args.reorg_delay
+    logging_path = args.logging_path
+    loglevel = args.loglevel
+    use_cached_events = args.use_cached_events
+    run_data_validator = args.run_data_validator
+    randomizer = args.randomizer
+    limit_bancor3_flashloan_tokens = args.limit_bancor3_flashloan_tokens
+    default_min_profit_gas_token = args.default_min_profit_gas_token
+    timeout = args.timeout
+    target_tokens = args.target_tokens
+    replay_from_block = args.replay_from_block
+    tenderly_fork_id = args.tenderly_fork_id
+    tenderly_event_exchanges = args.tenderly_event_exchanges
+    increment_time = args.increment_time
+    increment_blocks = args.increment_blocks
+    blockchain = args.blockchain
+    pool_data_update_frequency = args.pool_data_update_frequency
+    use_specific_exchange_for_target_tokens = args.use_specific_exchange_for_target_tokens
+    prefix_path = args.prefix_path
+    version_check_frequency = args.version_check_frequency
+    self_fund = args.self_fund
+    read_only = args.read_only
+
 
     if replay_from_block or tenderly_fork_id:
         polling_interval, reorg_delay, use_cached_events = handle_replay_from_block(
