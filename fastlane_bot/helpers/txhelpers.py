1906a691add1ed2c8009510ab232d9c547d6b178
Lesigh-3100 <kevin@bancor.network>
2024-02-05 17:37:59 +0200

    Update txhelpers.py

--- fastlane_bot/helpers/txhelpers.py
+++ fastlane_bot/helpers/txhelpers.py
@@ -1001,15 +1001,31 @@ class TxHelpers:
         returns: Decimal
             The total fee (in gas token) for the l1 gas fee
         """
-        ethereum_base_fee = await self.ConfigObj.GAS_ORACLE_CONTRACT.caller.basefee()
-        fixed_overhead = await self.ConfigObj.GAS_ORACLE_CONTRACT.caller.l1FeeOverhead()
-        dynamic_overhead = await self.ConfigObj.GAS_ORACLE_CONTRACT.caller.l1FeeScalar()
+        ethereum_base_fee = await self._get_layer_one_gas_price()
+        fixed_overhead = await self._get_layer_one_fee_overhead()
+        dynamic_overhead = await self._get_layer_one_fee_scalar()
         zero_bytes, non_zero_bytes = count_bytes(signed_transaction["rawTransaction"])
         tx_data_gas = zero_bytes * 4 + non_zero_bytes * 16
         tx_total_gas = (tx_data_gas + fixed_overhead) * dynamic_overhead
         l1_data_fee = tx_total_gas * ethereum_base_fee
         ## Dividing by 10 ** 24 because dynamic_overhead is returned in PPM format, and to convert this from WEI format to decimal format (10 ** 18).
         return Decimal(f"{l1_data_fee}e-24")
+
+    async def _get_layer_one_gas_price(self):
+        """
+        Returns the layer one base fee from the Layer 2 gas oracle for Optimism blockchains
+        """
+        return self.ConfigObj.GAS_ORACLE_CONTRACT.caller.basefee()
+    async def _get_layer_one_fee_overhead(self):
+        """
+        Returns the layer one fee overhead from the Layer 2 gas oracle for Optimism blockchains
+        """
+        return self.ConfigObj.GAS_ORACLE_CONTRACT.caller.l1FeeOverhead()
+    async def _get_layer_one_fee_scalar(self):
+        """
+        Returns the layer one fee overhead from the Layer 2 gas oracle for Optimism blockchains
+        """
+        return self.ConfigObj.GAS_ORACLE_CONTRACT.caller.l1FeeScalar()
 
 
 
