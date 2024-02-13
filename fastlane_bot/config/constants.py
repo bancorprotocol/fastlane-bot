b521c4d83da53f7db58480d90042e26bd5ad3bfa
Mike Casale <46603283+mikewcasale@users.noreply.github.com>
2024-02-13 11:33:04 -0800

    Update constants.py

--- fastlane_bot/config/constants.py
+++ fastlane_bot/config/constants.py
@@ -6,8 +6,7 @@ Licensed under MIT License.
 """
 
 FLASHLOAN_FEE_MAP = {
-    # should be in the form of {network: fee * 1e6}
     "ethereum": 0,
     "coinbase_base": 0,
-    "fantom": 0.0003  # %
+    "fantom": 0.0003
 }
