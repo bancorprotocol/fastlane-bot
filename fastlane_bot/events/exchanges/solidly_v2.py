6caaa9b8a7b98fcc166b6e23e25f00d1575d2216
Lesigh-3100 <kevin@bancor.network>
2024-02-14 13:51:52 +0200

    Fix handling of Solidly Factory ABI for equalizer & scale

--- fastlane_bot/events/exchanges/solidly_v2.py
+++ fastlane_bot/events/exchanges/solidly_v2.py
@@ -11,7 +11,7 @@ from typing import List, Type, Tuple, Any
 from web3.contract import Contract
 
 from fastlane_bot.data.abi import SOLIDLY_V2_POOL_ABI, VELOCIMETER_V2_FACTORY_ABI, SOLIDLY_V2_FACTORY_ABI, \
-    VELOCIMETER_V2_POOL_ABI
+    VELOCIMETER_V2_POOL_ABI, SCALE_V2_FACTORY_ABI, EQUALIZER_V2_POOL_ABI
 from fastlane_bot.events.exchanges.base import Exchange
 from fastlane_bot.events.pools.base import Pool
 
@@ -41,11 +41,21 @@ class SolidlyV2(Exchange):
         self.pools[pool.state["address"]] = pool
 
     def get_abi(self):
-        return VELOCIMETER_V2_POOL_ABI if "velocimeter" in self.exchange_name else SOLIDLY_V2_POOL_ABI
+        if self.exchange_name == "velocimeter_v2":
+            return VELOCIMETER_V2_POOL_ABI
+        elif self.exchange_name in ["equalizer_v2"]:
+            return EQUALIZER_V2_POOL_ABI
+        else:
+            return SOLIDLY_V2_POOL_ABI
 
     @property
     def get_factory_abi(self):
-        return VELOCIMETER_V2_FACTORY_ABI if "velocimeter" in self.exchange_name else SOLIDLY_V2_FACTORY_ABI
+        if self.exchange_name == "velocimeter_v2":
+            return VELOCIMETER_V2_FACTORY_ABI
+        elif self.exchange_name in ["scale_v2", "equalizer_v2"]:
+            return SCALE_V2_FACTORY_ABI
+        else:
+            return SOLIDLY_V2_FACTORY_ABI
 
     def get_events(self, contract: Contract) -> List[Type[Contract]]:
         return [contract.events.Sync] if self.exchange_initialized else []
