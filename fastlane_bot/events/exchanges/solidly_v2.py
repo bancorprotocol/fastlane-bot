21f4b07af58ba6d2a3c75fd64100e3f5538aeb8d
Lesigh-3100 <kevin@bancor.network>
2024-02-14 16:58:37 +0200

    Update solidly_v2.py

--- fastlane_bot/events/exchanges/solidly_v2.py
+++ fastlane_bot/events/exchanges/solidly_v2.py
@@ -15,6 +15,14 @@ from fastlane_bot.data.abi import SOLIDLY_V2_POOL_ABI, VELOCIMETER_V2_FACTORY_AB
 from fastlane_bot.events.exchanges.base import Exchange
 from fastlane_bot.events.pools.base import Pool
 
+FEE_RESOLUTION_MAP = {
+    "velocimeter_v2": 10000,
+    "equalizer_v2": 10000,
+    "aerodrome_v2": 10000,
+    "velodrome_v2": 10000,
+    "scale_v2": 1000000000000000000,
+    "solidly_v2": 10000,
+}
 
 @dataclass
 class SolidlyV2(Exchange):
@@ -43,39 +51,68 @@ class SolidlyV2(Exchange):
     def get_abi(self):
         if self.exchange_name == "velocimeter_v2":
             return VELOCIMETER_V2_POOL_ABI
-        elif self.exchange_name in ["equalizer_v2"]:
+        elif self.exchange_name == "equalizer_v2":
             return EQUALIZER_V2_POOL_ABI
-        else:
+        elif self.exchange_name == "scale_v2":
             return SOLIDLY_V2_POOL_ABI
+        elif self.exchange_name == "aerodrome_v2":
+            return SOLIDLY_V2_POOL_ABI
+        elif self.exchange_name == "velodrome_v2":
+            return SOLIDLY_V2_POOL_ABI
+        elif self.exchange_name == "solidly_v2":
+            return SOLIDLY_V2_POOL_ABI
+        else:
+            raise self.WrongExchangeException(f"[exchanges/solidly_v2 get_Abi] Exchange {self.exchange_name} not in supported exchanges.")
+
 
     @property
     def get_factory_abi(self):
         if self.exchange_name == "velocimeter_v2":
             return VELOCIMETER_V2_FACTORY_ABI
-        elif self.exchange_name in ["scale_v2", "equalizer_v2"]:
+        elif self.exchange_name == "equalizer_v2":
             return SCALE_V2_FACTORY_ABI
-        else:
+        elif self.exchange_name == "scale_v2":
+            return SCALE_V2_FACTORY_ABI
+        elif self.exchange_name == "aerodrome_v2":
             return SOLIDLY_V2_FACTORY_ABI
+        elif self.exchange_name == "velodrome_v2":
+            return SOLIDLY_V2_FACTORY_ABI
+        elif self.exchange_name == "solidly_v2":
+            return SOLIDLY_V2_FACTORY_ABI
+        else:
+            raise self.WrongExchangeException(f"[exchanges/solidly_v2 get_Abi] Exchange {self.exchange_name} not in supported exchanges.")
 
     def get_events(self, contract: Contract) -> List[Type[Contract]]:
         return [contract.events.Sync] if self.exchange_initialized else []
 
     async def get_fee(self, address: str, contract: Contract) -> Tuple[str, float]:
-        if "velocimeter" in self.exchange_name:
+        if self.exchange_name == "velocimeter_v2":
             default_fee = await self.factory_contract.caller.getFee(address)
-            default_fee = float(default_fee) / 10000
-
-        elif self.exchange_name in ["scale_v2", "equalizer_v2"]:
+        elif self.exchange_name == "equalizer_v2":
             default_fee = await self.factory_contract.caller.getRealFee(address)
-            default_fee = float(default_fee) / 10 ** 18 if self.exchange_name == "scale_v2" else float(default_fee) / 10000
-        else:
+        elif self.exchange_name == "scale_v2":
+            default_fee = await self.factory_contract.caller.getRealFee(address)
+        elif self.exchange_name == "aerodrome_v2":
             is_stable = await contract.caller.stable()
             default_fee = await self.factory_contract.caller.getFee(address, is_stable)
-            default_fee = float(default_fee) / 10000
-        return str(default_fee), default_fee
+        elif self.exchange_name == "velodrome_v2":
+            is_stable = await contract.caller.stable()
+            default_fee = await self.factory_contract.caller.getFee(address, is_stable)
+        elif self.exchange_name == "solidly_v2":
+            is_stable = await contract.caller.stable()
+            default_fee = await self.factory_contract.caller.getFee(address, is_stable)
+        else:
+            raise self.WrongExchangeException(f"[exchanges/solidly_v2 get_Abi] Exchange {self.exchange_name} not in supported exchanges.")
+
+        fee_float = float(default_fee) / FEE_RESOLUTION_MAP.get(self.exchange_name)
+
+        return str(fee_float), fee_float
 
     async def get_tkn0(self, address: str, contract: Contract, event: Any) -> str:
         return await contract.functions.token0().call()
 
     async def get_tkn1(self, address: str, contract: Contract, event: Any) -> str:
         return await contract.functions.token1().call()
+
+    class WrongExchangeException(Exception):
+        pass
