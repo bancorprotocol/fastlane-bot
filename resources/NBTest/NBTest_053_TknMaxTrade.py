c6e77999711b4190db97c773f44b398b2d57f32f
Lesigh-3100 <kevin@bancor.network>
2024-03-07 15:41:43 +0200

    Fix test

--- resources/NBTest/NBTest_053_TknMaxTrade.py
+++ resources/NBTest/NBTest_053_TknMaxTrade.py
@@ -7,7 +7,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.15.2
+#       jupytext_version: 1.14.5
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -23,7 +23,7 @@ from dataclasses import asdict
 
 from fastlane_bot import Bot
 from fastlane_bot.events.exchanges import UniswapV2, UniswapV3, CarbonV1, BancorV3
-from fastlane_bot.helpers.routehandler import RouteStruct, maximize_last_trade_per_tkn
+from fastlane_bot.helpers.routehandler import maximize_last_trade_per_tkn
 from fastlane_bot.tools.cpc import ConstantProductCurve as CPC
 
 print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(CPC))
@@ -37,84 +37,36 @@ from fastlane_bot.testing import *
 plt.rcParams['figure.figsize'] = [12,6]
 from fastlane_bot import __VERSION__
 require("3.0", __VERSION__)
+
+
 # -
 
 # # Maximize Last Trade Per TKN [NBTest053]
 
 # ## Test_use_0_for_sourceAmount
 
-# +
+def test_maximize_last_trade_per_tkn():
+    route_struct_0 = [                    
+                            {"sourceAmount": 10, "sourceToken": "bob_tkn", "minReturn": 10, "targetToken": "fred_tkn"},
+                            {"sourceAmount": 10, "sourceToken": "bob_tkn", "minReturn": 10, "targetToken": "fred_tkn"},
+                            {"sourceAmount": 10, "sourceToken": "fred_tkn", "minReturn": 10, "targetToken": "grog_tkn"},
+                            {"sourceAmount": 10, "sourceToken": "fred_tkn", "minReturn": 10, "targetToken": "grog_tkn"},
+                            {"sourceAmount": 10, "sourceToken": "grog_tkn", "minReturn": 10, "targetToken": "bob_tkn"},
+                            {"sourceAmount": 10, "sourceToken": "grog_tkn", "minReturn": 10, "targetToken": "bob_tkn"},
 
+                    ]
 
-ti1 = RouteStruct(
-platformId=8,
-sourceToken="0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-targetToken="0x6b175474e89094c44da98b954eedeac495271d0f",
-sourceAmount=62211456000000000000,
-minTargetAmount=99890009252976620728523,
-deadline=1702409775,
-customAddress="0x6b175474e89094c44da98b954eedeac495271d0f",
-customInt=0,
-customData="0x"
-)
+    source_token = route_struct_0[0]["sourceToken"]
+    maximize_test_result = maximize_last_trade_per_tkn(route_struct_0)
 
-ti2 = RouteStruct(platformId=4,
-sourceToken="0x6b175474e89094c44da98b954eedeac495271d0f",
-targetToken="0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-sourceAmount=99890009252976620728523,
-minTargetAmount=62256092760867779024,
-deadline=1702409775,
-customAddress="0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-customInt=3000,
-customData="0x")
+    assert len(maximize_test_result) == len(route_struct_0)
 
+    for trade in maximize_test_result:
+        if trade["sourceToken"] == source_token:
+            assert trade["sourceAmount"] > 0
 
-ti3 = RouteStruct(
-platformId=8,
-sourceToken="0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-targetToken="0x6b175474e89094c44da98b954eedeac495271d0f",
-sourceAmount=62211456000000000000,
-minTargetAmount=99890009252976620728523,
-deadline=1702409775,
-customAddress="0x6b175474e89094c44da98b954eedeac495271d0f",
-customInt=0,
-customData="0x"
-)
-ti4 = RouteStruct(
-platformId=8,
-sourceToken="0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-targetToken="0x6b175474e89094c44da98b954eedeac495271d0f",
-sourceAmount=62211456000000000000,
-minTargetAmount=99890009252976620728523,
-deadline=1702409775,
-customAddress="0x6b175474e89094c44da98b954eedeac495271d0f",
-customInt=0,
-customData="0x"
-)
-
-ti5 = RouteStruct(platformId=4,
-sourceToken="0x6b175474e89094c44da98b954eedeac495271d0f",
-targetToken="0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-sourceAmount=99890009252976620728523,
-minTargetAmount=62256092760867779024,
-deadline=1702409775,
-customAddress="0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-customInt=3000,
-customData="0x")
-
-instructions = [ti1, ti2]
-instructions2 = [asdict(ti3), asdict(ti4), asdict(ti5)]
- 
-max_trade_route_struct = maximize_last_trade_per_tkn(instructions)
-max_trade_route_struct2 = maximize_last_trade_per_tkn(instructions2)
-
-assert max_trade_route_struct[0].sourceAmount == 0, f"[NBTest_053] sourceAmount expected 0, actual: {max_trade_route_struct[0].sourceAmount}"
-assert max_trade_route_struct[1].sourceAmount == 0, f"[NBTest_053] sourceAmount expected 0, actual: {max_trade_route_struct[1].sourceAmount}"
-assert max_trade_route_struct2[0]["sourceAmount"] == 62211456000000000000, f"[NBTest_053] sourceAmount expected 0, actual: {max_trade_route_struct2[0]['sourceAmount']}"
-assert max_trade_route_struct2[1]["sourceAmount"] == 0, f"[NBTest_053] sourceAmount expected 0, actual: {max_trade_route_struct2[1]['sourceAmount']}"
-assert max_trade_route_struct2[2]["sourceAmount"] == 0, f"[NBTest_053] sourceAmount expected 0, actual: {max_trade_route_struct2[2]['sourceAmount']}"
-
-# -
+    assert maximize_test_result[3]["sourceAmount"] == 0
+    assert maximize_test_result[5]["sourceAmount"] == 0
 
 
 
