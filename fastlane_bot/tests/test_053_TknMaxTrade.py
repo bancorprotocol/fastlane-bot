7d4640b8d186761ba74aa86c26cb40cd8148df0b
Lesigh-3100 <110377282+Lesigh-3100@users.noreply.github.com>
2024-03-14 15:23:22 +0200

    Fix self funding (#413)
    * Fix self funding
    
    * Fix test
    
    * Update tx contract construction & gas handling
    
    * Reformatting & update type in txhelpers
    
    * Update txhelpers.py
    
    * Update test_053_TknMaxTrade.py

--- fastlane_bot/tests/test_053_TknMaxTrade.py
+++ fastlane_bot/tests/test_053_TknMaxTrade.py
@@ -38,79 +38,31 @@ require("3.0", __VERSION__)
 # File      test_053_TknMaxTrade.py
 # Segment   Test_use_0_for_sourceAmount
 # ------------------------------------------------------------
-def test_test_use_0_for_sourceamount():
-# ------------------------------------------------------------
-    
-    # +
-    
-    
-    ti1 = RouteStruct(
-    platformId=8,
-    sourceToken="0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-    targetToken="0x6b175474e89094c44da98b954eedeac495271d0f",
-    sourceAmount=62211456000000000000,
-    minTargetAmount=99890009252976620728523,
-    deadline=1702409775,
-    customAddress="0x6b175474e89094c44da98b954eedeac495271d0f",
-    customInt=0,
-    customData="0x"
-    )
-    
-    ti2 = RouteStruct(platformId=4,
-    sourceToken="0x6b175474e89094c44da98b954eedeac495271d0f",
-    targetToken="0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-    sourceAmount=99890009252976620728523,
-    minTargetAmount=62256092760867779024,
-    deadline=1702409775,
-    customAddress="0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-    customInt=3000,
-    customData="0x")
-    
-    
-    ti3 = RouteStruct(
-    platformId=8,
-    sourceToken="0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-    targetToken="0x6b175474e89094c44da98b954eedeac495271d0f",
-    sourceAmount=62211456000000000000,
-    minTargetAmount=99890009252976620728523,
-    deadline=1702409775,
-    customAddress="0x6b175474e89094c44da98b954eedeac495271d0f",
-    customInt=0,
-    customData="0x"
-    )
-    ti4 = RouteStruct(
-    platformId=8,
-    sourceToken="0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-    targetToken="0x6b175474e89094c44da98b954eedeac495271d0f",
-    sourceAmount=62211456000000000000,
-    minTargetAmount=99890009252976620728523,
-    deadline=1702409775,
-    customAddress="0x6b175474e89094c44da98b954eedeac495271d0f",
-    customInt=0,
-    customData="0x"
-    )
-    
-    ti5 = RouteStruct(platformId=4,
-    sourceToken="0x6b175474e89094c44da98b954eedeac495271d0f",
-    targetToken="0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-    sourceAmount=99890009252976620728523,
-    minTargetAmount=62256092760867779024,
-    deadline=1702409775,
-    customAddress="0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-    customInt=3000,
-    customData="0x")
-    
-    instructions = [ti1, ti2]
-    instructions2 = [asdict(ti3), asdict(ti4), asdict(ti5)]
-     
-    max_trade_route_struct = maximize_last_trade_per_tkn(instructions)
-    max_trade_route_struct2 = maximize_last_trade_per_tkn(instructions2)
-    
-    assert max_trade_route_struct[0].sourceAmount == 0, f"[NBTest_053] sourceAmount expected 0, actual: {max_trade_route_struct[0].sourceAmount}"
-    assert max_trade_route_struct[1].sourceAmount == 0, f"[NBTest_053] sourceAmount expected 0, actual: {max_trade_route_struct[1].sourceAmount}"
-    assert max_trade_route_struct2[0]["sourceAmount"] == 62211456000000000000, f"[NBTest_053] sourceAmount expected 0, actual: {max_trade_route_struct2[0]['sourceAmount']}"
-    assert max_trade_route_struct2[1]["sourceAmount"] == 0, f"[NBTest_053] sourceAmount expected 0, actual: {max_trade_route_struct2[1]['sourceAmount']}"
-    assert max_trade_route_struct2[2]["sourceAmount"] == 0, f"[NBTest_053] sourceAmount expected 0, actual: {max_trade_route_struct2[2]['sourceAmount']}"
+def test_maximize_last_trade_per_tkn():
+    route_struct_0 = [
+                            {"sourceAmount": 10, "sourceToken": "bob_tkn", "minReturn": 10, "targetToken": "fred_tkn"},
+                            {"sourceAmount": 10, "sourceToken": "bob_tkn", "minReturn": 10, "targetToken": "fred_tkn"},
+                            {"sourceAmount": 10, "sourceToken": "fred_tkn", "minReturn": 10, "targetToken": "grog_tkn"},
+                            {"sourceAmount": 10, "sourceToken": "fred_tkn", "minReturn": 10, "targetToken": "grog_tkn"},
+                            {"sourceAmount": 10, "sourceToken": "grog_tkn", "minReturn": 10, "targetToken": "bob_tkn"},
+                            {"sourceAmount": 10, "sourceToken": "grog_tkn", "minReturn": 10, "targetToken": "bob_tkn"},
+
+                    ]
+
+    source_token = route_struct_0[0]["sourceToken"]
+    maximize_test_result = maximize_last_trade_per_tkn(route_struct_0)
+
+    assert len(maximize_test_result) == len(route_struct_0)
+
+    for trade in maximize_test_result:
+        if trade["sourceToken"] == source_token:
+            assert trade["sourceAmount"] > 0
+    assert maximize_test_result[0]["sourceAmount"] == 10
+    assert maximize_test_result[1]["sourceAmount"] == 10
+    assert maximize_test_result[2]["sourceAmount"] == 10
+    assert maximize_test_result[3]["sourceAmount"] == 0
+    assert maximize_test_result[4]["sourceAmount"] == 10
+    assert maximize_test_result[5]["sourceAmount"] == 0
     
     # -
     
