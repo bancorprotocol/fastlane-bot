8fb1dc3dae765512d641b3da2eb72edbfdacb64e
barak manos <>
2024-03-18 19:57:10 +0200

    Rename 'id' to 'token_type'

--- fastlane_bot/helpers/carbon_trade_splitter.py
+++ fastlane_bot/helpers/carbon_trade_splitter.py
@@ -25,16 +25,16 @@ def split_carbon_trades(cfg, trade_instructions: list[TradeInstruction]) -> list
             pool = trade_instruction.db.get_pool(cid=str(tx["cid"]).split("-")[0])
 
             if cfg.NATIVE_GAS_TOKEN_ADDRESS in pool.get_tokens:
-                id = cfg.NATIVE_GAS_TOKEN_ADDRESS
+                token_type = cfg.NATIVE_GAS_TOKEN_ADDRESS
             elif cfg.WRAPPED_GAS_TOKEN_ADDRESS in pool.get_tokens:
-                id = cfg.WRAPPED_GAS_TOKEN_ADDRESS
+                token_type = cfg.WRAPPED_GAS_TOKEN_ADDRESS
             else:
-                id = cfg.ZERO_ADDRESS
+                token_type = cfg.ZERO_ADDRESS
 
-            tx["tknin"] = _get_token_address(cfg, id, trade_instruction.tknin)
-            tx["tknout"] = _get_token_address(cfg, id, trade_instruction.tknout)
+            tx["tknin"] = _get_token_address(cfg, token_type, trade_instruction.tknin)
+            tx["tknout"] = _get_token_address(cfg, token_type, trade_instruction.tknout)
 
-            exchange_id = pool.exchange_name + id
+            exchange_id = pool.exchange_name + token_type
             if exchange_id in carbon_exchanges:
                 carbon_exchanges[exchange_id].append(tx)
             else:
@@ -58,9 +58,9 @@ def split_carbon_trades(cfg, trade_instructions: list[TradeInstruction]) -> list
 
     return new_trade_instructions
 
-def _get_token_address(cfg, id: str, token_address: str) -> str:
-    if id == cfg.NATIVE_GAS_TOKEN_ADDRESS and token_address == cfg.WRAPPED_GAS_TOKEN_ADDRESS:
+def _get_token_address(cfg, token_type: str, token_address: str) -> str:
+    if token_type == cfg.NATIVE_GAS_TOKEN_ADDRESS and token_address == cfg.WRAPPED_GAS_TOKEN_ADDRESS:
         return cfg.NATIVE_GAS_TOKEN_ADDRESS
-    if id == cfg.WRAPPED_GAS_TOKEN_ADDRESS and token_address == cfg.NATIVE_GAS_TOKEN_ADDRESS:
+    if token_type == cfg.WRAPPED_GAS_TOKEN_ADDRESS and token_address == cfg.NATIVE_GAS_TOKEN_ADDRESS:
         return cfg.WRAPPED_GAS_TOKEN_ADDRESS
     return token_address
