cef262ddcd2c311ba290f0ecc646954b8b3a9c4f
sklbancor <109073706+sklbancor@users.noreply.github.com>
2024-04-03 14:45:22 +0100

    Updating tests (flashloan tokens)

--- fastlane_bot/tests/test_051_BalancerFlashloans.py
+++ fastlane_bot/tests/test_051_BalancerFlashloans.py
@@ -129,7 +129,7 @@ bot.db.remove_zero_liquidity_pools()
 bot.db.remove_unsupported_exchanges()
 tokens = bot.db.get_tokens()
 ADDRDEC = {t.address: (t.address, int(t.decimals)) for t in tokens if not math.isnan(t.decimals)}
-flashloan_tokens = bot.setup_flashloan_tokens(None)
+flashloan_tokens = bot.RUN_FLASHLOAN_TOKENS
 CCm = bot.setup_CCm(None)
 pools = db.get_pool_data_with_tokens()
 
