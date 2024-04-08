51c14a8aa51a5e98ebdf5e5139d5de13df272334
barak manos <>
2024-04-08 17:05:14 +0300

    Clean-up the new ABIs, and implement for uniswap_v3 a paradigm similar to solidly_v2

--- fastlane_bot/events/exchanges/uniswap_v3.py
+++ fastlane_bot/events/exchanges/uniswap_v3.py
@@ -11,10 +11,36 @@ from typing import List, Type, Tuple, Any
 from web3.contract import Contract
 
 from fastlane_bot.config.constants import AGNI_V3_NAME, PANCAKESWAP_V3_NAME, FUSIONX_V3_NAME, ECHODEX_V3_NAME, \
-    SECTA_V3_NAME, NILE_V3_NAME
+    SECTA_V3_NAME, SUSHISWAP_V3_NAME, UNISWAP_V3_NAME, BASESWAP_V3_NAME, HORIZON_V3_NAME, METAVAULT_V3_NAME, \
+    BUTTER_V3_NAME, CLEOPATRA_V3_NAME, NILE_V3_NAME
 from fastlane_bot.data.abi import UNISWAP_V3_POOL_ABI, UNISWAP_V3_FACTORY_ABI, PANCAKESWAP_V3_POOL_ABI, NILE_V3_POOL_ABI
 from fastlane_bot.events.exchanges.base import Exchange
 from fastlane_bot.events.pools.base import Pool
+
+
+async def _get_fee_1(contract: Contract) -> int:
+    return await contract.functions.fee().call()
+
+
+async def _get_fee_2(contract: Contract) -> int:
+    return await contract.functions.currentFee().call()
+
+
+EXCHANGE_INFO = {
+    SUSHISWAP_V3_NAME: {"pool_abi": UNISWAP_V3_POOL_ABI, "fee_function": _get_fee_1},
+    UNISWAP_V3_NAME: {"pool_abi": UNISWAP_V3_POOL_ABI, "fee_function": _get_fee_1},
+    BASESWAP_V3_NAME: {"pool_abi": UNISWAP_V3_POOL_ABI, "fee_function": _get_fee_1},
+    HORIZON_V3_NAME: {"pool_abi": UNISWAP_V3_POOL_ABI, "fee_function": _get_fee_1},
+    METAVAULT_V3_NAME: {"pool_abi": UNISWAP_V3_POOL_ABI, "fee_function": _get_fee_1},
+    BUTTER_V3_NAME: {"pool_abi": UNISWAP_V3_POOL_ABI, "fee_function": _get_fee_1},
+    CLEOPATRA_V3_NAME: {"pool_abi": UNISWAP_V3_POOL_ABI, "fee_function": _get_fee_1},
+    PANCAKESWAP_V3_NAME: {"pool_abi": PANCAKESWAP_V3_POOL_ABI, "fee_function": _get_fee_1},
+    AGNI_V3_NAME: {"pool_abi": PANCAKESWAP_V3_POOL_ABI, "fee_function": _get_fee_1},
+    FUSIONX_V3_NAME: {"pool_abi": PANCAKESWAP_V3_POOL_ABI, "fee_function": _get_fee_1},
+    ECHODEX_V3_NAME: {"pool_abi": PANCAKESWAP_V3_POOL_ABI, "fee_function": _get_fee_1},
+    SECTA_V3_NAME: {"pool_abi": PANCAKESWAP_V3_POOL_ABI, "fee_function": _get_fee_1},
+    NILE_V3_NAME: {"pool_abi": NILE_V3_POOL_ABI, "fee_function": _get_fee_2},
+}
 
 
 @dataclass
@@ -31,11 +57,7 @@ class UniswapV3(Exchange):
         self.pools[pool.state["address"]] = pool
 
     def get_abi(self):
-        if self.exchange_name in [PANCAKESWAP_V3_NAME, AGNI_V3_NAME, FUSIONX_V3_NAME, ECHODEX_V3_NAME, SECTA_V3_NAME]:
-            return PANCAKESWAP_V3_POOL_ABI
-        elif self.exchange_name == NILE_V3_NAME:
-            return NILE_V3_POOL_ABI
-        return UNISWAP_V3_POOL_ABI
+        return EXCHANGE_INFO[self.exchange_name]["pool_abi"]
 
     @property
     def get_factory_abi(self):
@@ -45,11 +67,8 @@ class UniswapV3(Exchange):
         return [contract.events.Swap] if self.exchange_initialized else []
 
     async def get_fee(self, address: str, contract: Contract) -> Tuple[str, float]:
-        fee = await contract.functions.fee().call()
+        fee = await EXCHANGE_INFO[self.exchange_name]["fee_function"](contract)
         fee_float = float(fee) / 1e6
-        if self.exchange_name == NILE_V3_NAME:
-            fee_float = await contract.functions.currentFee().call()
-
         return fee, fee_float
 
     async def get_tkn0(self, address: str, contract: Contract, event: Any) -> str:
