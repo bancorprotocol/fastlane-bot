d4de9fa4d378d0c324473cf154d69ee8b265a989
Lesigh-3100 <kevin@bancor.network>
2024-04-09 14:25:36 +0300

    Update run_blockchain_terraformer.py

--- run_blockchain_terraformer.py
+++ run_blockchain_terraformer.py
@@ -1286,4 +1286,4 @@ def terraform_blockchain(network_name: str, web3: Web3 = None, async_web3: Async
 
 
 #terraform_blockchain(network_name="mantle", save_tokens=True)
-terraform_blockchain(network_name="linea", save_tokens=True)
+#terraform_blockchain(network_name="linea", save_tokens=True)
