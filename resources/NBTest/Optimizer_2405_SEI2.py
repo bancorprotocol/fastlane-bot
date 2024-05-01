65a13c10fd157ea8f4d040b194c480e16e9be33a
sklbancor <109073706+sklbancor@users.noreply.github.com>
2024-05-01 22:13:32 +0100

    Optimizer Notebooks

--- resources/NBTest/Optimizer_2405_SEI2.py
+++ resources/NBTest/Optimizer_2405_SEI2.py
@@ -25,15 +25,16 @@ except:
 CPC = ConstantProductCurve
 
 import pandas as pd
+import math as m
 
-print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(SimplePair))
-print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(CPC))
-print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(CPCContainer))
-print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(PairOptimizer))
+#print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(SimplePair))
+#print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(CPC))
+#print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(CPCContainer))
+#print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(PairOptimizer))
 print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(MargPOptimizer))
 # -
 
-# # Optimizer Testing (TEMPLATE)
+# # Optimizer Testing (SEI2, May 2024)
 
 # This is a light workbook allowing to look at issues that may arise when running the optimizer on a specific set of curves. 
 #
@@ -44,7 +45,6 @@ print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(MargPOptimizer))
 # - add the missing token addresses to the `TOKENS` dict below
 # - provide consistent values for `PSTART`
 # - run the workbook
-# - if the import statement fails, ensure `fastlane_bot` is on the path, or create symlink to `tools`
 
 # ### >> Enter curves
 #
@@ -55,16 +55,6 @@ print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(MargPOptimizer))
 #         ConstantProductCurve(k=6.160500599566333e+18, x=11099999985.149971, x_act=0, ...),
 #     ...
 #     ]
-
-CurvesRaw = [
-    CPC.from_pk(pair="ETH_/USDP_",  cid="ETH/USDP1",  p=2000,   k=1),   # 1E+$2000 @ 2000
-    # CPC.from_pk(pair="ETH_/USDP_",  cid="ETH/USDP1",  p=2000,   k=1*2000*1),   # 1E+$2000 @ 2000
-    # CPC.from_pk(pair="ETH_/USDP_",  cid="ETH/USDP1",  p=3000,   k=1*3000*1),   # 1E+$2000 @ 2000
-    CPC.from_pk(pair="LINK_/USDP_", cid="LINK/USDP1", p=  10,   k=100*1000), # 200L+$2000 @ 10
-    CPC.from_pk(pair="LINK_/USDP_", cid="LINK/USDP1", p=  15,   k=100*1500), # 200L+$2000 @ 10
-    # CPC.from_pk(pair="ETH_/LINK_",  cid="ETH/LINK1", p=  210,   k=1*210*0.0000000000001),    # ~1E @ 210
-]
-CCRaw = CPCContainer(CurvesRaw)
 
 # +
 # CurvesRaw = [
@@ -78,6 +68,16 @@ CCRaw = CPCContainer(CurvesRaw)
 # CCRaw = CPCContainer(CurvesRaw)
 # -
 
+CurvesRaw = [
+    ConstantProductCurve(k=27518385.40998667, x=1272.2926367501436, x_act=0, y_act=2000.9999995236503, alpha=0.5, pair='0x514910771AF9Ca656af840dff83E8264EcF986CA/0x8E870D67F660D95d5be530380D0eC0bd388289E1', cid='0x425d5d4ad7243f88d9f4cde8da52863b45af1f64e05bede1299909bcaa6c52d1-0', fee=2000, descr='carbon_v1 0x514910771AF9Ca656af840dff83E8264EcF986CA\\/0x8E870D67F660D95d5be530380D0eC0bd388289E1 2000', constr='carb', params={'exchange': 'carbon_v1', 'y': 2000.9999995236503, 'yint': 2000.9999995236503, 'A': 0.38144823884371704, 'B': 3.7416573867739373, 'pa': 16.99999999999995, 'pb': 13.99999999999997}),
+    #ConstantProductCurve(k=6.160500599566333e+18, x=11099999985.149971, x_act=0, y_act=55.50000002646446, alpha=0.5, pair='0x8E870D67F660D95d5be530380D0eC0bd388289E1/0x514910771AF9Ca656af840dff83E8264EcF986CA', cid='0x425d5d4ad7243f88d9f4cde8da52863b45af1f64e05bede1299909bcaa6c52d1-1', fee=2000, descr='carbon_v1 0x514910771AF9Ca656af840dff83E8264EcF986CA\\/0x8E870D67F660D95d5be530380D0eC0bd388289E1 2000', constr='carb', params={'exchange': 'carbon_v1', 'y': 55.50000002646446, 'yint': 55.50000002646446, 'A': 0, 'B': 0.22360678656963742, 'pa': 0.04999999999999889, 'pb': 0.04999999999999889}),
+    CPC.from_pk(p= 230,  k=1*230*(100**2),  cid="ETH/LINK", pair='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE/0x514910771AF9Ca656af840dff83E8264EcF986CA', ), 
+    CPC.from_pk(p=3220,  k=1*3220*(100**2), cid="ETH/USDP", pair='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE/0x8E870D67F660D95d5be530380D0eC0bd388289E1', ), 
+]
+CCRaw = CPCContainer(CurvesRaw)
+
+help(ConstantProductCurve.from_pk)
+
 # ### >> Enter prices
 #
 # Provide current prices (`pstart`) here, in the format
@@ -88,22 +88,13 @@ CCRaw = CPCContainer(CurvesRaw)
 #         '0xEeee...': 1
 #     }
 #     
-# The price numeraire does not matter as long as they are all in the same numeraire. All tokens must be present. Additional tokens can be added and will be ignored. The keys in the dict here must correspond exactly to the 
-# keys that are used in the pairs in the curves above.
+# The price numeraire does not matter as long as they are all in the same numeraire. All tokens must be present. Additional tokens can be added and will be ignored. 
 
 PRICES_RAW = {
-    'ETH_': 1950, 
-    'LINK_':  11, 
-    'USDP_':   1
+    '0x8E870D67F660D95d5be530380D0eC0bd388289E1': 0.0003087360213944532, 
+    '0x514910771AF9Ca656af840dff83E8264EcF986CA': 0.004372219704179475, 
+    '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE': 1
 }
-
-# +
-# PRICES_RAW = {
-#     '0x8E870D67F660D95d5be530380D0eC0bd388289E1': 0.0003087360213944532, 
-#     '0x514910771AF9Ca656af840dff83E8264EcF986CA': 0.004372219704179475, 
-#     '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE': 1
-# }
-# -
 
 # ### >> Enter tokens
 #
@@ -115,27 +106,17 @@ PRICES_RAW = {
 #         "0xEeee...": "ETH",
 #     }
 #     
-# All tokens must be present. Additional tokens will be ignored. You must also provide the `TARGET_TOKEN_RAW`,
-# for example by picking from the token list
-
-# +
-# TOKENS = {
-#     "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE": "ETH",
-#     "0x514910771AF9Ca656af840dff83E8264EcF986CA": "LINK",
-#     "0x8E870D67F660D95d5be530380D0eC0bd388289E1": "USDP",
-# }
-
-# TARGET_TOKEN_RAW = list(TOKENS)[0]
-# TARGET_TOKEN_RAW
+# All tokens must be present. Additional tokens will be ignored. You must also provide the `TARGET_TOKEN` (default: first token of `TOKENS`)
+#
 
 # +
 TOKENS = {
-    "ETH_": "ETH",
-    "LINK_": "LINK",
-    "USDP_": "USDP",
+    "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE": "ETH",
+    "0x514910771AF9Ca656af840dff83E8264EcF986CA": "LINK",
+    "0x8E870D67F660D95d5be530380D0eC0bd388289E1": "USDP",
 }
 
-TARGET_TOKEN_RAW = list(TOKENS)[-1]
+TARGET_TOKEN_RAW = list(TOKENS)[0]
 TARGET_TOKEN_RAW
 # -
 
@@ -200,9 +181,16 @@ PRICES = {TOKENS[addr]:price for addr, price in PRICES_RAW.items()}
 TARGET_TOKEN = TOKENS[TARGET_TOKEN_RAW]
 PRICES
 
-# Here you can change the preference order of numeraire tokens (in a pair, the one with the lower number will be chosen as numeraire; tokens not present here are considered to have higher numbers; draw means alphabetic order). 
-#
-# The specific code below ensures that in ETH/LINK, LINK is the quote token and ETH the base token. As LINK is not in the list, otherwise this would be the other way round.
+
+def p(pair=None, *, tknb=None, tknq=None, prices=None):
+    "price of tknb in terms of tknq"
+    if not pair is None:
+        tknb, tknq = pair.split("/")
+    p = prices or PRICES
+    return p[tknb]/p[tknq]
+
+
+# The code below ensures that in ETH/LINK, LINK is the quote token and ETH the base token (for better price displays)
 
 SimplePair.NUMERAIRE_TOKENS["LINK"] = SimplePair.NUMERAIRE_TOKENS["ETH"] - 1
 #SimplePair.NUMERAIRE_TOKENS
@@ -247,10 +235,24 @@ curvedata = [dict(
 curvedf = pd.DataFrame(curvedata)
 curvedf
 
+# - The arbitrageable curve is #0 that buys LINK against USDP on the way down from `17 to 14`, at an average price of `15.4 LINK/USDP`; liquidity is `0.6 ETH ~ 2000 USDP`
+# - The triangle is made up via two massive curves (20 ETH each) with price points at `230 ETH/LINK` and `3220 ETH/USDP` respectively, yielding an effective `ETH/LINK of 14`
+# - The average profit is `10%`, on a volume of `2000 USDP or 0.6 ETH`, ie `200 USD or 0.06 ETH`
+# - That is close enough to the actual `result 0.05 ETH` (see below), keeping in mind that there is still some slippage on the big curves
+
+0.6*3220, m.sqrt(17*14), 3220/230, 15.4/14-1
+
 # For reference, the CID dataframe `ciddf` (separate because the field is too long; can be joined to `curvedf` via index)
 
 ciddf = pd.DataFrame([dict(cid=c.cid) for c in CC])
 ciddf
+
+# +
+#help(CC[0])
+
+# +
+#help(CC[0].pairo)
+# -
 
 # ## MargPOptimizer
 
@@ -266,52 +268,8 @@ r = O.optimize(sfc=TARGET_TOKEN, params=dict(
 ))
 r
 
-raise
+r.result, r.targettkn, -r.result*3400
 
-# ### Explanation
-#
-# Convergence criterium is "relative" with given epsilon using L2 norm (unit does not matter here)
-#
-#     [margp_optimizer] crit=rel (eps=1e-06, unit=1, norm=L2)
-#
-# Base token is `USDP`; the prices and their inverses shown in order `ETH`, `LINK`
-#
-#     [margp_optimizer] USDP <- ETH, LINK
-#     [margp_optimizer] p    1,950.00, 11.00
-#     [margp_optimizer] 1/p  0.00, 0.09
-#     
-# Jacobian explainer: quote token us USDP
-# `dETH/d%pLINK` means the change in ETH (in ETH units) when the `LINK/USDP` price changes by 1%
-#
-#     [margp_optimizer]
-#     ============= JACOBIAN% =============>>>
-#     tknq USDP
-#     dETH/d%pETH, dLINK/d%pETH
-#     dETH/d%pLINK, dLINK/d%pLINK
-#     <<<============= JACOBIAN% =============
-#
-# Actual Jacobian in the same format as the explainer
-#
-#     [margp_optimizer]
-#     ============= JACOBIAN% =============>>>
-#     [[-0.01045332  0.005415  ]
-#      [ 0.95994502 -1.90864222]]
-#     <<<============= JACOBIAN% =============
-#     
-# Results of cycle 0
-#
-#     [margp_optimizer]
-#     ========== cycle 0 =======>>>
-#     USDP <- ETH, LINK
-#     dtkn   0.101, -26.364
-#     log p0 [3.290034611362518, 1.0413926851582251]
-#     d logp [ 0.01472711 -0.05228352]
-#     log p  [3.30476172 0.98910917]
-#     p_t      (2017.2592691917046, 9.752347514669442) USDP
-#     p        2,017.26, 9.75
-#     1/p      0.00, 0.10
-#     crit     5.43e-02 [1; L2], eps=1e-06, c/e=5e+04]
-#     dtkn_d   {'ETH': 0.10113974590360497, 'USDP': 72.4594621534543, 'LINK': -26.36377863280171}
-#     <<<========== cycle 0 =======
+
 
 
