a4be88578d1af0c279d293647b6465178ee73c3f
sklbancor <109073706+sklbancor@users.noreply.github.com>
2024-05-04 16:44:05 +0100

    module init files (curves, optimizer)

--- fastlane_bot/tools/__init__.py
+++ fastlane_bot/tools/__init__.py
@@ -11,3 +11,15 @@ __VERSION_DATE__ = '07/Feb/2024'
 __AUTHOR__ = 'Stefan K Loesch'
 __COPYRIGHT__ = 'Bprotocol foundation 2023-24'
 __LICENSE__ = 'MIT'
+
+
+from .curves import SimplePair
+Pair = SimplePair 
+# TODO-RELEASE-202405: clean up Pair/SimplePair before final release
+
+from .curves import CurveBase
+from .curves import ConstantProductCurve
+from .curves import CurveContainer
+
+from .optimizer import MargPOptimizer
+from .optimizer import PairOptimizer
