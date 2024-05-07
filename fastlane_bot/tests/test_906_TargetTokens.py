66b3439965a60d096368aff349217c94f13240bb
Platon Floria <platon.floria@gmail.com>
2024-05-07 22:15:51 +0200

    cleanup

--- fastlane_bot/tests/test_906_TargetTokens.py
+++ fastlane_bot/tests/test_906_TargetTokens.py
@@ -11,11 +11,14 @@
 """
 This module contains the tests which ensure the target_tokens parameter is respected.
 """
+import os
+import subprocess
+
+from arb_optimizer import ConstantProductCurve as CPC
+from arb_optimizer.curves import T
+
 from fastlane_bot import Bot
-from fastlane_bot.tools import ConstantProductCurve as CPC
 from fastlane_bot.events.exchanges import UniswapV2, UniswapV3,  CarbonV1, BancorV3
-import subprocess, os, sys
-import pytest
 print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(CPC))
 print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(Bot))
 print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(UniswapV2))
@@ -26,10 +29,6 @@ from fastlane_bot.testing import *
 plt.rcParams['figure.figsize'] = [12,6]
 from fastlane_bot import __VERSION__
 require("3.0", __VERSION__)
-
-
-
-from fastlane_bot.tools.curves import T
 
 
 def find_main_py():
