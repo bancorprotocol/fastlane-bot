3feca37bdeaf47f3fc1fbbc6778f58ee42886af2
Platon Floria <platon.floria@gmail.com>
2024-05-08 21:45:14 +0200

    feat: dedicated Exchange types for distinct solidly forks

---
+++ fastlane_bot/events/exchanges/solidly_v2/lynex_v2.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from typing import Tuple
+
+from web3.contract import AsyncContract
+
+from fastlane_bot.data.abi import LYNEX_V2_FACTORY_ABI
+from .base import SolidlyV2
+
+
+@dataclass
+class LynexV2(SolidlyV2):
+    @property
+    def get_factory_abi(self):
+        return LYNEX_V2_FACTORY_ABI
+
+    async def get_fee(self, address: str, contract: AsyncContract) -> Tuple[str, float]:
+        fee = await self.factory_contract.caller.getFee(await contract.caller.stable())
+        fee_float = float(fee) / 10 ** 4
+        return str(fee_float), fee_float
+
+    def get_pool_function(self, factory_contract):
+        return factory_contract.functions.getPair
