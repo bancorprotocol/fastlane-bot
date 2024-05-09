2da81c86d2613912dd3600e95a18fed652c4f8eb
Platon Floria <platon.floria@gmail.com>
2024-05-10 01:14:51 +0300

    fix: tests

--- fastlane_bot/tests/test_073_TestPoolFinder.py
+++ fastlane_bot/tests/test_073_TestPoolFinder.py
@@ -37,9 +37,7 @@ def test_find_unsupported_triangles():
 
 def test_extract_pairs():
     # Sample data for testing
-    uni_v2_exchanges = ["bob_ex"]
     uni_v3_exchanges = ["fred_ex"]
-    solidly_v2_exchanges = ["george_ex", "moose_ex"]
     flashloan_tokens = ["BNT"]
     pools = [
         {"exchange_name": "CarbonX", "tkn0_address": "WBTC", "tkn1_address": "BNT"},
@@ -54,10 +52,17 @@ def test_extract_pairs():
     expected_carbon_pairs = {frozenset(('WBTC', 'BNT')), frozenset(('WETH', 'USDC'))}
     expected_other_pairs = {frozenset(('WETH', 'USDT')), frozenset(('USDC', 'WBTC'))}
     #expected_carbon_pairs = [('WBTC', 'BNT'), ('WETH', 'USDC')]
-    pool_finder = PoolFinder(uni_v2_forks=uni_v2_exchanges, uni_v3_forks=uni_v3_exchanges, solidly_v2_forks=solidly_v2_exchanges, carbon_forks=carbon_forks, flashloan_tokens=flashloan_tokens)
+    pool_finder = PoolFinder(
+        uni_v3_forks=uni_v3_exchanges,
+        carbon_forks=carbon_forks,
+        flashloan_tokens=flashloan_tokens,
+        exchanges={},
+        web3=None,
+        multicall_address=""
+    )
 
     # Call the function with test data
-    carbon_pairs, other_pairs = pool_finder._extract_pairs(pools, carbon_forks)
+    carbon_pairs, other_pairs = pool_finder._extract_pairs(pools)
 
     # Assert the results are as expected
     assert frozenset(carbon_pairs[0]) in expected_carbon_pairs
@@ -67,31 +72,3 @@ def test_extract_pairs():
     #assert other_pairs == expected_other_pairs
     assert len(carbon_pairs) == 2
     assert len(other_pairs) == 2
-
-def test_sort_exchanges():
-    exchange_pools = {
-        "bob_ex": ["1", "2", "3"],
-        "fred_ex": ["4", "5"],
-        "george_ex": ["6", "7", "8"],
-        "moose_ex": ["9", "10", "11", "12"]
-    }
-
-    uni_v2_exchanges = ["bob_ex"]
-    uni_v3_exchanges = ["fred_ex"]
-    solidly_v2_exchanges = ["george_ex", "moose_ex"]
-    uni_v2_pools, uni_v3_pools, solidly_v2_pools = PoolFinder._sort_exchange_pools(ex_pools=exchange_pools, uni_v2_forks=uni_v2_exchanges, uni_v3_forks=uni_v3_exchanges, solidly_v2_forks=solidly_v2_exchanges)
-
-
-    assert len(uni_v2_pools.keys()) == 3
-    assert len(uni_v3_pools.keys()) == 2
-    assert len(solidly_v2_pools.keys()) == 7
-
-    assert uni_v2_pools["1"] == "bob_ex"
-    assert uni_v3_pools["5"] == "fred_ex"
-    assert solidly_v2_pools["7"] == "george_ex"
-    assert solidly_v2_pools["9"] == "moose_ex"
-
-
-
-
-
