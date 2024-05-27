bdc5855cabc767345eae0d33b0c5d50c5a73a529
Nicholas Welch <nick@bancor.network>
2024-05-27 13:16:49 +1000

    new pstart handling

--- fastlane_bot/modes/base_triangle.py
+++ fastlane_bot/modes/base_triangle.py
@@ -8,6 +8,7 @@ Defines the base class for triangular arbitrage finder modes
 All rights reserved.
 Licensed under MIT.
 """
+import random
 from typing import Any, List, Dict
 from fastlane_bot.tools.cpc import CPCContainer
 from fastlane_bot.tools.optimizer import MargPOptimizer
@@ -22,7 +23,7 @@ class ArbitrageFinderTriangleBase(ArbitrageFinderBase):
             try:
                 container = CPCContainer(miniverse)
                 optimizer = MargPOptimizer(container)
-                params = get_params(self.CCm, container.tokens(), src_token)
+                params = get_params(self, container, container.tokens(), src_token)
                 optimization = optimizer.optimize(src_token, params=params)
                 trade_instructions_dic = optimization.trade_instructions(optimizer.TIF_DICTS)
                 trade_instructions_df = optimization.trade_instructions(optimizer.TIF_DFAGGR)
@@ -45,16 +46,36 @@ class ArbitrageFinderTriangleBase(ArbitrageFinderBase):
 
         return {"combos": combos, "arb_opps": sorted(arb_opps, key=lambda arb_opp: arb_opp["profit"], reverse=True)}
 
-def get_params(CCm, dst_tokens, src_token):
+def get_params(self, CCm, dst_tokens, src_token):
+    # For a triangle, the pstart of each dst_token is derived from its rate vs the src_token.
+    # Since Carbon orders can contain diverse prices independent of external market prices, and
+    # we require that the pstart be on the Carbon curve to get successful optimizer runs,
+    # then for Carbon orders only we must randomize the pstart from the list of available Carbon curves.
+    # Random selection chosen as opposed to iterating over all possible combinations.
+
+    # ASSUMPTIONS: There must be a complete triangle arb available i.e. src_token->dst_token1->dst_token2->src_token
+    sort_sequence = ['bancor_v2','bancor_v3'] + self.ConfigObj.UNI_V2_FORKS + self.ConfigObj.UNI_V3_FORKS
     pstart = {src_token: 1}
     for dst_token in [token for token in dst_tokens if token != src_token]:
-        CC = CCm.bytknx(dst_token).bytkny(src_token)
+        curves = list(CCm.bytknx(dst_token).bytkny(src_token))
+        CC = CPCContainer(custom_sort(curves, sort_sequence, self.ConfigObj.CARBON_V1_FORKS))
         if CC:
-            pstart[dst_token] = CC[0].p
+            if CC[0].params['exchange'] in self.ConfigObj.CARBON_V1_FORKS: #only carbon curve options left
+                pstart[dst_token] = random.choice(CC).p
+            else:
+                pstart[dst_token] = CC[0].p
         else:
-            CC = CCm.bytknx(src_token).bytkny(dst_token)
+            curves = list(CCm.bytknx(src_token).bytkny(dst_token))
+            CC = CPCContainer(custom_sort(curves, sort_sequence, self.ConfigObj.CARBON_V1_FORKS))
             if CC:
-                pstart[dst_token] = 1 / CC[0].p
+                if CC[0].params['exchange'] in self.ConfigObj.CARBON_V1_FORKS: #only carbon curve options left
+                    pstart[dst_token] = 1/(random.choice(CC).p)
+                else:
+                    pstart[dst_token] = 1 / CC[0].p
             else:
                 return None
     return {"pstart": pstart}
+
+def custom_sort(data, sort_sequence, carbon_v1_forks):
+    sort_order = {key: index for index, key in enumerate(sort_sequence) if key not in carbon_v1_forks}
+    return sorted(data, key=lambda item: float('inf') if item.params['exchange'] in carbon_v1_forks else sort_order.get(item.params['exchange'], float('inf')))
