032eea6f414a80ea0842fe25757b90792cf42f2e
barak manos <>
2024-05-29 13:17:33 +0300

    Fix command-line tests

--- fastlane_bot/tests/test_048_RespectFlashloanTokensClickParam.py
+++ fastlane_bot/tests/test_048_RespectFlashloanTokensClickParam.py
@@ -7,15 +7,13 @@
 # ------------------------------------------------------------
 
 
-
 """
-This module contains the tests which ensure that the flashloan tokens click parameters are respected.
+This module contains the tests the `multi_pairwise_all` arb-mode with the `flashloan_tokens` parameter.
 """
 from fastlane_bot import Bot
 from fastlane_bot.tools.cpc import ConstantProductCurve as CPC
 from fastlane_bot.events.exchanges import UniswapV2, UniswapV3,  CarbonV1, BancorV3
-import subprocess, os, sys
-import pytest
+import subprocess, os
 print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(CPC))
 print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(Bot))
 print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(UniswapV2))
@@ -26,8 +24,6 @@ from fastlane_bot.testing import *
 plt.rcParams['figure.figsize'] = [12,6]
 from fastlane_bot import __VERSION__
 require("3.0", __VERSION__)
-
-
 
 
 def find_main_py():
@@ -50,37 +46,24 @@ def find_main_py():
             cwd = new_cwd
        
        
-def run_command(arb_mode):
-    
-    # Find the correct path to main.py
-    main_script_path = find_main_py()
-    print(f"Found main.py in {main_script_path}")
-    main_script_path = os.path.normpath(main_script_path + "/main.py")
-
-    # Run the command
-    cmd = [
-        "python",
-        main_script_path,
-        f"--arb_mode={arb_mode}",
-        "--default_min_profit_gas_token=0.001",
-        "--limit_bancor3_flashloan_tokens=False",
-        "--use_cached_events=False",
-        "--timeout=1",
-        "--loglevel=DEBUG",
-        "--flashloan_tokens='0x1F573D6Fb3F13d689FF844B4cE37794d79a7FF1C,0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE,0xAa6E8127831c9DE45ae56bB1b0d4D4Da6e5665BD'",
-        "--blockchain=ethereum"
-    ]
-
-    result = subprocess.run(cmd, text=True, capture_output=True, check=True)
-    print(result.stderr)
-
-
 # ------------------------------------------------------------
 # Test      048
 # File      test_048_RespectFlashloanTokensClickParam.py
 # Segment   Test flashloan_tokens is Respected
 # ------------------------------------------------------------
-def test_test_flashloan_tokens_is_respected():
+def test_test_multi_pairwise_all_with_flashloan_tokens():
 # ------------------------------------------------------------
     
-    run_command("multi_pairwise_all")
+    cmd = [
+        "python",
+        find_main_py() + "/main.py",
+        f"--arb_mode=multi_pairwise_all",
+        "--default_min_profit_gas_token=0.001",
+        "--use_cached_events=False",
+        "--timeout=120",
+        "--flashloan_tokens='0x1F573D6Fb3F13d689FF844B4cE37794d79a7FF1C,0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE,0xAa6E8127831c9DE45ae56bB1b0d4D4Da6e5665BD'",
+        "--blockchain=ethereum"
+    ]
+
+    result = subprocess.run(cmd, text=True, capture_output=True, check=True)
+    assert result.returncode == 0
